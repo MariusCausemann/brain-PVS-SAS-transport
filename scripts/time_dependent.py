8d948d82bfba0a1ba63cf4a195e779bb9e9939ae
Marius Causemann <mariuscausemann@yahoo.de>
2023-10-30 22:24:56 +0100

    configure only diffusion setup

--- scripts/time_dependent.py
+++ scripts/time_dependent.py
@@ -3,6 +3,7 @@ from dolfin import *
 import xii 
 from solver import * 
 import os
+import numpy as np
 
 if __name__ == '__main__':
    
@@ -15,14 +16,49 @@ if __name__ == '__main__':
     sas = Mesh()
     with XDMFFile('../mesh/volmesh/mesh.xdmf') as f:
         f.read(sas)
-        sas_subdomains = MeshFunction('size_t', sas, 3,0)
+        sas_subdomains = MeshFunction('size_t', sas, 3, 0)
         f.read(sas_subdomains, 'label')
 
-    Ds = pcws_constant(sas_subdomains, {1: Constant(20),  # sas
-                                        2: Constant(30)})
+    assert np.allclose(np.unique(sas_subdomains.array()), [1,2])
+    [m.scale(1e-3) for m in [sas, vein, artery]] # scale from mm to m
+
+    dt = 60
+    T = 60*60*1 # 4h
+    num_timesteps = int(T / dt)
+
+    m = (0.17, 0.21, 0.1)
+    inlet_radius = 0.040
+
+    inlet = CompiledSubDomain("on_boundary && " + 
+                            "+ (x[0] - m0)*(x[0] - m0) " + 
+                            "+ (x[1] - m1)*(x[1] - m1) " + 
+                            "+ (x[2] - m2)*(x[2] - m2) < r*r",
+                            m0=m[0], m1=m[1], m2=m[2], r=inlet_radius)
+    
+    bm = MeshFunction("size_t", sas, 2, 0)
+    inlet.mark(bm, 1)
+    
+    boundary_concentration = Constant(1)
+
+    mm2m = 1e-3
+    ecs_vol = 0.2
+    Ds = pcws_constant(sas_subdomains, {1: Constant(3.8*1e-4 * mm2m),  # csf
+                                        2: Constant(1.2*1e-4 * mm2m*ecs_vol) # parenchyma
+                                        })
+        
+    Da = Constant(3.8*1e-4 * mm2m) 
+    Dv = Constant(3.8*1e-4 * mm2m)
+    xi = Constant(1e-3)
+
+    velocity_a = Constant(0.0)
+    velocity_v = Constant(0.0)
+
+    fa = Constant(0.0) 
+    fv = Constant(0.0)
+    gSAS = Constant(0.0)
 
     V = FunctionSpace(sas, 'CG', 1)
-    Qa = FunctionSpace(artery, 'CG',1)
+    Qa = FunctionSpace(artery, 'CG', 1)
     Qv = FunctionSpace(vein, 'CG', 1)
     W = [V, Qa, Qv]
 
@@ -45,21 +81,12 @@ if __name__ == '__main__':
     dx_v = Measure('dx', domain=vein)
     vein_shape = xii.Circle(radius=vein_radii, degree=20)
     uv, vv = (xii.Average(x, vein, vein_shape) for x in (u, v))
-    dt = 0.001 
-    num_timesteps = 300
-    T  = num_timesteps*dt  
+
     a = xii.block_form(W, 2)
-    # Ds = Constant(2.0) 
-    Da = Constant(1.0) 
-    Dv = Constant(1.0)
-    velocity_a = Constant(1.0)
-    velocity_v = Constant(1.0)
-    fa = Constant(10.0) 
-    fv = Constant(10.0)
+
 
     hF, nF, gamma_Nitsche = CellDiameter(sas), FacetNormal(sas), Constant(100)
-    gSAS = Constant(0.0)
-    xi = Constant(17.0)
+
     a[0][0] = (1/dt)*inner(u,v)*dx + Ds*inner(grad(u), grad(v))*dx + xi*inner(ua, va)*dx_a + xi*inner(uv, vv)*dx_v
     a[0][1] = -xi*inner(pa, va)*dx_a
     a[0][2] = -xi*inner(pv, vv)*dx_v
@@ -80,9 +107,9 @@ if __name__ == '__main__':
     AA, bb = map(xii.ii_assemble, (a, L))
 
     
-    V_bcs  =  [DirichletBC(V,Constant(0.0), DomainBoundary())]
-    Qa_bcs = []
-    Qv_bcs = []
+    V_bcs  =  [DirichletBC(V, boundary_concentration, inlet)]
+    Qa_bcs = [DirichletBC(Qa, boundary_concentration, inlet)]
+    Qv_bcs = [DirichletBC(Qv, boundary_concentration, inlet)]
     W_bcs = [V_bcs, Qa_bcs, Qv_bcs]
 
     AA, _, bc_apply_b = xii.apply_bc(AA, bb, bcs=W_bcs, return_apply_b=True)
@@ -96,8 +123,8 @@ if __name__ == '__main__':
     #opts.setValue('ksp_converged_reason', None)
     #opts.setValue('ksp_norm_type', 'unpreconditioned')
     #opts.setValue('ksp_monitor_true_residual', None)
-    opts.setValue('ksp_rtol', 1E-40)
-    opts.setValue('ksp_atol', 1E-12)   # |AX-b| < 1E-12
+    #opts.setValue('ksp_rtol', 1E-40)
+    opts.setValue('ksp_atol', 1E-12)   # |AX-b| < 1E-
     opts.setValue('pc_type', 'hypre')
     opts.setValue('ksp_initial_guess_nonzero', 1)
 
@@ -112,36 +139,15 @@ if __name__ == '__main__':
     vtkfile_1 = File(results_dir + 'uh_sas.pvd') 
     vtkfile_2 = File(results_dir + 'uh_artery.pvd')
     vtkfile_3 = File(results_dir + 'uh_vein.pvd') 
-    vtkfile_4 = File(results_dir + 'uh_real_sas.pvd') 
-    vtkfile_5 = File(results_dir + 'uh_hemi1.pvd') 
-    #vtkfile_6 = File('uh_hemi2.pvd') 
 
-    u_i.rename("SAS", "time")
-    pa_i.rename("Arteries", "time")
-    pv_i.rename("Veins", "time")
+    u_i.rename("c", "time")
+    pa_i.rename("c", "time")
+    pv_i.rename("c", "time")
 
     vtkfile_1 << (u_i, float(0.0))
     vtkfile_2 << (pa_i, float(0.0))
     vtkfile_3 << (pv_i, float(0.0))
 
-    sas_mesh = SubMesh(sas, sas_subdomains, 1)
-    V_sas = FunctionSpace(sas_mesh, V.ufl_element())
-    hemi1_mesh = SubMesh(sas, sas_subdomains,2) 
-    V_hemi1    = FunctionSpace(hemi1_mesh, V.ufl_element())
-    #hemi2_mesh = SubMesh(sas, sas_subdomains,3) 
-    #V_hemi2    = FunctionSpace(hemi2_mesh, V.ufl_element())
-    uh_sas = Function(V_sas)
-    uh_sas.rename('SAS', 'time')
-    uh_hemi1 = Function(V_hemi1)
-    uh_hemi1.rename('Hemispheres', 'time')
-
-    #uh_hemi2 = Function(V_hemi2)
-   # uh_hemi2.rename('Hemispheres', 'time')
-
-    uh_hemi1.assign(interpolate(u_i, V_hemi1)) 
-    #uh_hemi2.assign(interpolate(u_i, V_hemi2)) 
-    vtkfile_5 << (uh_hemi1, 0.0 )
-   # vtkfile_6 << (uh_hemi2, 0.0)
     wh = xii.ii_Function(W)
     x_ = A_.createVecLeft()
     while t < T: 
@@ -158,17 +164,10 @@ if __name__ == '__main__':
         pa_i.assign(wh[1]) 
         pv_i.assign(wh[2])
 
-        uh_sas.assign(interpolate(wh[0], V_sas))
-        uh_hemi1.assign(interpolate(wh[0], V_hemi1))
-        #uh_hemi2.assign(interpolate(wh[0], V_hemi2))
-
         t += dt 
-        wh[0].rename("SAS", "time")
-        wh[1].rename("Arteries", "time")
-        wh[2].rename("Veins", "time")
+        wh[0].rename("c", "time")
+        wh[1].rename("c", "time")
+        wh[2].rename("c", "time")
         vtkfile_1 << (wh[0],float(t)) 
         vtkfile_2 << (wh[1],float(t)) 
-        vtkfile_3 << (wh[2],float(t)) 
-        vtkfile_4 << (uh_sas,float(t)) 
-        vtkfile_5 << (uh_hemi1,float(t)) 
-        #vtkfile_6 << (uh_hemi2,float(t))    
+        vtkfile_3 << (wh[2],float(t))
