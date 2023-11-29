1aba1afce717b9b1b192e978b3f9b52b115a5be4
Marius Causemann <mariuscausemann@yahoo.de>
2023-11-29 16:52:05 +0100

    transfer marker from vol to network

--- scripts/time_dependent.py
+++ scripts/time_dependent.py
@@ -8,6 +8,10 @@ import typer
 from pathlib import Path
 from plotting_utils import read_config
 
+def write(sols, files, t):
+    for s,f in zip(sols, files):
+        f.write(s, t)
+
 def run_simulation(configfile: str):
 
     config = read_config(configfile)
@@ -50,7 +54,14 @@ def run_simulation(configfile: str):
     vein_radii.vector()[:] *= 1e-3
     artery_radii.vector()[:] *= 1e-3
 
-    
+
+    artery_shape = xii.Circle(radius=artery_radii, degree=20,)
+    vein_shape = xii.Circle(radius=vein_radii, degree=20)
+    artmarker = volmarker_to_networkmarker(vol_subdomains, artery, artery_shape)
+    veinmarker = volmarker_to_networkmarker(vol_subdomains, vein, vein_shape)
+    artmarker.rename("marker", "time")
+    veinmarker.rename("marker", "time")
+    vol_subdomains.rename("marker", "time")
     inlet = CompiledSubDomain("on_boundary && " + 
                             "+ (x[0] - m0)*(x[0] - m0) " + 
                             "+ (x[1] - m1)*(x[1] - m1) " + 
@@ -60,17 +71,18 @@ def run_simulation(configfile: str):
     
     bm = MeshFunction("size_t", sas, 2, 0)
     inlet.mark(bm, 1)
-    
-    phi = pcws_constant(vol_subdomains, {1: Constant(1),  # csf
-                                         2: Constant(ecs_share) # parenchyma
-                                        })
+    xi_dict = {0:Constant(0), 1: Constant(pvs_csf_permability), 
+               2: Constant(pvs_parenchyma_permability)}
+    phi_dict = {1: Constant(1),  2: Constant(ecs_share)}
+    phi = pcws_constant(vol_subdomains, phi_dict)
     Ds = pcws_constant(vol_subdomains, {1: Constant(sas_diffusion),  # csf
                                         2: Constant(parenchyma_diffusion) # parenchyma
                                         })
-    xi = pcws_constant(vol_subdomains, {1: Constant(pvs_csf_permability),  # csf
-                                        2: Constant(pvs_parenchyma_permability) # parenchyma
-                                        })
-    xi.set_allow_extrapolation(True)
+    phi_a = pcws_constant(artmarker, phi_dict)
+    phi_v = pcws_constant(veinmarker, phi_dict)
+    xi_a = pcws_constant(artmarker, xi_dict)
+    xi_v = pcws_constant(veinmarker, xi_dict)
+
     Da = Constant(arterial_pvs_diffusion) 
     Dv = Constant(venous_pvs_diffusion)
 
@@ -101,59 +113,28 @@ def run_simulation(configfile: str):
     artery_shape = xii.Circle(radius=artery_radii, degree=20,)
     ua, va = (xii.Average(x, artery, artery_shape) for x in (u, v))
 
-
-    ##
-    S3d = FunctionSpace(sas, "DG", 0)
-    S1d = FunctionSpace(artery, "DG", 0)
-    foo = interpolate(Constant(1), S3d)
-    bar = TestFunction(S1d)
-    
-    vec = xii.ii_assemble((1/CellVolume(artery))*inner(xii.Average(foo, artery, artery_shape), bar)*dx_a)
-    arterial_mask = Function(S1d)
-    coefs = vec.get_local(); coefs[coefs < 0.99] = 0.0; vec.set_local(coefs)
-    arterial_mask.vector()[:] = vec 
-                                                                        
-
+                                               
     dx_v = Measure('dx', domain=vein)
-    vein_shape = xii.Circle(radius=vein_radii, degree=20)
     uv, vv = (xii.Average(x, vein, vein_shape) for x in (u, v)) 
-    ##
-    S3d = FunctionSpace(sas, "DG", 0)
-    S1d = FunctionSpace(vein, "DG", 0)
-    foo = interpolate(Constant(1), S3d)
-    bar = TestFunction(S1d)
-    
-    vec = xii.ii_assemble((1/CellVolume(vein))*inner(xii.Average(foo, vein, vein_shape), bar)*dx_v)
-    coefs = vec.get_local(); coefs[coefs < 0.99] = 0.0; vec.set_local(coefs)
-
-    venous_mask = Function(S1d)
-    venous_mask.vector()[:] = vec
-
-    phia = xii.Average(phi, artery, artery_shape)
-    phiv = xii.Average(phi, vein,vein_shape)
-    xia = xii.Average(xi, artery, artery_shape)
-    xiv = xii.Average(xi, vein, vein_shape)
 
     a = xii.block_form(W, 2)
-    xia_int = interpolate(xi, Qa)
-    xiv_int = interpolate(xi, Qv)
 
     a[0][0] = phi*(1/dt)*inner(u,v)*dx + phi*Ds*inner(grad(u), grad(v))*dx \
-            + phia*xia*arterial_mask*(2*pi*perm_artery)*inner(ua, va)*dx_a \
-            + phiv*xiv*venous_mask*(2*pi*perm_vein)*inner(uv, vv)*dx_v
+            + phi_a*xi_a*(2*pi*perm_artery)*inner(ua, va)*dx_a \
+            + phi_v*xi_v*(2*pi*perm_vein)*inner(uv, vv)*dx_v
 
-    a[0][1] = -phia*xia*arterial_mask*(2*pi*perm_artery)*inner(pa, va)*dx_a
-    a[0][2] = -phiv*xiv*venous_mask*(2*pi*perm_vein)*inner(pv, vv)*dx_v
+    a[0][1] = -phi_a*xi_a*(2*pi*perm_artery)*inner(pa, va)*dx_a
+    a[0][2] = -phi_v*xi_v*(2*pi*perm_vein)*inner(pv, vv)*dx_v
 
-    a[1][0] =-phia*xia*arterial_mask*(2*pi*perm_artery)*inner(qa, ua)*dx_a
+    a[1][0] =-phi_a*xi_a*(2*pi*perm_artery)*inner(qa, ua)*dx_a
     a[1][1] = (1/dt)*area_artery*inner(pa,qa)*dx + Da*area_artery*inner(grad(pa), grad(qa))*dx \
             - area_artery*inner(pa, dot(velocity_a,grad(qa)[0]))*dx  \
-            + xia_int*arterial_mask*(2*pi*perm_artery)*inner(pa, qa)*dx
+            + xi_a*(2*pi*perm_artery)*inner(pa, qa)*dx
 
-    a[2][0] = -phiv*xiv*venous_mask*(2*pi*perm_vein)*inner(qv, uv)*dx_v
+    a[2][0] = -phi_v*xi_v*(2*pi*perm_vein)*inner(qv, uv)*dx_v
     a[2][2] = (1/dt)*area_vein*inner(pv,qv)*dx + Dv*area_vein*inner(grad(pv), grad(qv))*dx \
             - area_vein*inner(pv, dot(velocity_v,grad(qv)[0]))*dx \
-            + xiv_int*venous_mask*(2*pi*perm_vein)*inner(pv, qv)*dx 
+            + xi_v*(2*pi*perm_vein)*inner(pv, qv)*dx 
 
     L = xii.block_form(W, 1)
     L[0]  = phi*(1/dt)*inner(u_i,v)*dx 
@@ -210,14 +191,8 @@ def run_simulation(configfile: str):
     pvdvenes = File(results_dir + f'{modelname}_venes.pvd') 
     files = (pvdsas, pvdarteries, pvdvenes)
 
-    def write(sols, files, t):
-        for s,f in zip(sols, files):
-            f.write(s, t)
-
     write((u_i, pa_i, pv_i), files, 0.0)
-    write((xia_int, xiv_int), (pvdarteries, pvdvenes), 0.0)
-
-
+    write((vol_subdomains, artmarker, veinmarker), files, 0.0)
     wh = xii.ii_Function(W)
     x_ = A_.createVecLeft()
     while t < T: 
