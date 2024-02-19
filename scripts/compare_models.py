e97a9388fa8b1185b206854f3df2b1d7e2eb881a
Marius Causemann <mariuscausemann@yahoo.de>
2024-02-19 16:50:13 +0100

    adjust color limits
    Former-commit-id: 09d92094e72c5988bb9c185345fa10b17ecbc9fd

--- scripts/compare_models.py
+++ scripts/compare_models.py
@@ -1,25 +1,45 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import ImageGrid
+import numpy as np
 import typer
 from typing import List
-from plotting_utils import time_str, read_config
+from plotting_utils import time_str, read_config, compute_ranges, compute_diff_ranges
+from generate_plot import plot_model
+from create_diff_plot import plot_model_diff
 
-def compare_models(modela:str, modelb:str, times:List[int]):
+crop_dict = {"overview":lambda img : img[100:-10,140:-140],
+             "overview_bottom":lambda img : img[100:-100,140:-140],
+             "detail":lambda img : img,
+            "detail_bottom":lambda img : img,
+             }
+padding = {"overview":0, "detail":0.05}
+percentile = 95
+
+def compare_models(modela:str, modelb:str, type:str, cmax:float, diffmax:float, times:List[int]):
 
     fig = plt.figure(figsize=(5., 9.), frameon=True)
-    grid = ImageGrid(fig, 111, nrows_ncols=(len(times), 3), axes_pad=0.0)
+    grid = ImageGrid(fig, 111, nrows_ncols=(len(times), 3), axes_pad=padding[type])
+    if not cmax:
+        rangea = compute_ranges(modela, times, percentile,)
+        rangeb = compute_ranges(modelb, times, percentile)
+        cmax = np.max([r[1] for r in rangea.values()] + [r[1] for r in rangeb.values()])
+    if not diffmax:
+        rangediff = compute_diff_ranges(modela, modelb, times, percentile)
+        rmin = np.min([r[0] for r in rangediff.values()])
+        rmax = np.max([r[1] for r in rangediff.values()])
+        diffmax = max(abs(rmin), abs(rmax))
     for i, t in enumerate(times):
-        for j,m in enumerate([modela, modelb, "diff"]):
-            fn = f"plots/{m}/{m}_{t}.png"
+        for j,m in enumerate([modela, modelb,"diff"]):
             if m=="diff":
-                fn = f"plots/comparisons/{modela}_{modelb}/{modela}_{modelb}_diff_{t}.png"
-            img = plt.imread(fn)
+                img = plot_model_diff(modela, modelb, t, type, cmax=diffmax)
+            else:
+                img = plot_model(m, t, type, cmax=cmax)
             ax = grid.axes_row[i][j]
             ax.axis('off')
             if i + 1==len(times):
-                ax.imshow(img[100:-10,140:-140])
+                ax.imshow(crop_dict[type](img))
             else:
-                ax.imshow(img[100:-100,140:-140])
+                ax.imshow(crop_dict[type + "_bottom"](img))
 
         ax = grid.axes_row[i][0]
         ax.text(-0.08, 0.5, f"{time_str(t)} h", rotation=90, va="center",
@@ -32,7 +52,7 @@ def compare_models(modela:str, modelb:str, times:List[int]):
         ax.text(0.5, 1.05, m, ha="center", transform=ax.transAxes, fontsize=6)
 
 
-    plt.savefig(f"plots/comparisons/{modela}_{modelb}/{modela}_{modelb}.png",
+    plt.savefig(f"plots/comparisons/{modela}_{modelb}/{modela}_{modelb}_{type}.png",
                  dpi=300, bbox_inches="tight",)
 
 if __name__ == "__main__":
