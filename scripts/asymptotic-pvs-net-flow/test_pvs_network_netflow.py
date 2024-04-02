Add fix for sign to be checked further.
Former-commit-id: 7159053369120e1062fddab8f0b8e4497df0c878

scripts/asymptotic-pvs-net-flow/test_pvs_network_netflow.py
@@ -24,7 +24,7 @@ def single_bifurcation_data():
     # Peristaltic wave parameters: wave length lmbda and (angular) wave number k
     f = 1.0                 # frequency (Hz = 1/s)
     omega = 2*np.pi*f       # Angular frequency (Hz)
-    lmbda = 2.0             # mm    
+    lmbda = 10.0             # mm    
     k = 2*np.pi/lmbda       # wave number (1/mm)
     varepsilon = 0.1        # AU 
         