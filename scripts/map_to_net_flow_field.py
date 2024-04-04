9ef1523d22de76cb98751e0a6d2ec82b4b751e7e
Marie E. Rognes <meg@simula.no>
2024-04-04 17:23:01 +0200

    Compute shortest paths between supply nodes.

--- scripts/map_to_net_flow_field.py
+++ scripts/map_to_net_flow_field.py
@@ -1,57 +1,53 @@
+import os.path
+
 import networkx as nx
-import dolfin
+import dolfin as df
 import numpy as np
 
 from solver import read_vtk_network
-from collections import defaultdict
-from itertools import repeat
 
-def map_vasculature(fname):
+def map_vasculature(fname, output):
 
-    # Read network information from 'fname', and extract radii and
-    # roots information as FEniCS cell and vertex functions,
-    # respectively.
-    mesh, radii, roots = read_vtk_network(fname)
+    # Read network information from 'fname' -- argh, don't rescale it.
+    print("Reading from %s" % fname)
+    mesh, radii, roots = read_vtk_network(fname, rescale_mm2m=False)
+    mesh.init()
 
     # Use networkx directed graph representation of mesh for convenience.
     G = nx.DiGraph()
     G.add_edges_from(mesh.cells())
     degrees = np.array([G.degree(i) for i in range(G.number_of_nodes())])
-    print("Vertices where degree == 1: ", np.where(degrees == 1)[0])
+    print("Number of nodes = ", mesh.num_vertices())
+    print("Number of edges = ", mesh.num_cells())
 
-    # Reduce the vascular network to a bifurcating tree and keep a map
-    # from tree element index to vessel segment indices.
+    # FIXME: Identify the network supply nodes (Currently identified
+    # by a semi-visual inspection.)
+    (r0, r1, r2) = [4094, 7220, 7974]
 
-    print(mesh.num_vertices())
-    print(mesh.num_cells())
-    exit()
-          
-    
-    # Start with the root node with lowest vertex index.
-    first_roots = np.where(roots.array() == 2)[0]
-    print("Vertices where root == 2: ", first_roots)
-    more_roots = np.where(roots.array() == 1)[0]
-    print("Vertices where root == 1: ", more_roots)
-    #print(mesh.cells()[0])
-        
-    exit()
+    # Compute shortest path from each of the supply nodes (r0, r1, r2)
+    # to the other(s) via Dijkstra's algorithm. (Undirected graph to
+    # make sure that the path is found.)
+    H = G.to_undirected()
+    p01 = nx.dijkstra_path(H, r0, r1)
+    p02 = nx.dijkstra_path(H, r0, r2)
+    p12 = nx.dijkstra_path(H, r1, r2)
 
-    # Reduce each subnetwork to bifurcating tree
-    #num_edges = mesh.num_cells()
-    #for c in dolfin.cells(mesh):
-    #    print(c.index())
-    #print("mesh.num_cells() = ", mesh.num_cells())
-    
-    print(mesh.cells())
-    print(radii.array())
-    print(roots.array())
+    # Store paths
+    print("Storing shortest paths to %s/..." % output)
+    mf = df.MeshFunction("size_t", mesh, 0, 0)
+    mf.array()[p01] = 1
+    df.File(os.path.join(output, "path_r0r1.pvd")) << mf
 
-    #print(roots, num_roots)
+    mf = df.MeshFunction("size_t", mesh, 0, 0)
+    mf.array()[p02] = 2
+    df.File(os.path.join(output, "path_r0r2.pvd")) << mf
 
-    
+    mf = df.MeshFunction("size_t", mesh, 0, 0)
+    mf.array()[p12] = 3
+    df.File(os.path.join(output, "path_r1r2.pvd")) << mf
+
 if __name__ == '__main__':
 
-    #map_vasculature("../mesh/networks/arteries_smooth.vtk")
-    map_vasculature("../mesh/networks/venes_smooth.vtk")
+    map_vasculature("../mesh/networks/arteries_smooth.vtk", "paths")
+    #map_vasculature("../mesh/networks/venes_smooth.vtk")
 
-    exit()
