2eca4cfe035ca6a18ed7465771c8b61757b948d0
Marie E. Rognes <meg@simula.no>
2024-04-09 14:55:52 +0200

    Start working on computing reduced/minimal graph consisting of bifurcations connected by edges. How to address degree > 3?
    Former-commit-id: 1e4ca3d323c6817fd61b88316d950db87785b189

--- scripts/map_to_net_flow_field.py
+++ scripts/map_to_net_flow_field.py
@@ -1,3 +1,6 @@
+# mamba activate pvs_transport_env
+
+from itertools import combinations
 import os.path
 
 import networkx as nx
@@ -6,11 +9,108 @@ import numpy as np
 
 from solver import read_vtk_network
 
+def compute_shortest_paths(mesh, H, roots, output="_tmp"):
+    # Find and mark shortest path from each of the nodes in G to the
+    # 'roots'.
+
+    # Helper function to compute path length/weight based on edge data
+    def path_length(G, path):
+        length = 0.0
+        for (i, _) in enumerate(path[:-1]):
+            length += G.get_edge_data(path[i], path[i+1])["weight"]
+        return length
+
+    # Create a vertex function that represents whether r0, r1 or r2 is
+    # the closest node for each (terminal) node
+    mf = df.MeshFunction("size_t", mesh, 0, 0)
+
+    # labels map (0, 1, ..., n_r) -> (index_0, index_1, ..., index_r)
+    labels = dict(zip(range(len(roots)), roots))
+
+    # Get all terminal nodes to begin with
+    nodes = [t for t in H if H.degree(t) == 1]
+
+    # ... and in addition all nodes in shortests paths betweeen
+    # pairs of roots
+    (r0, r1, r2) = roots
+    pairs = combinations(roots, 2) 
+    for (r0, r1) in pairs: 
+        path = nx.dijkstra_path(H, r0, r1)
+        nodes.extend(path)
+    nodes = set(nodes)
+
+    # Iterate over these nodes in an attempt to cover whole network
+    print("Computing nearest (weighted) supply node for %d nodes " % len(nodes))
+    for node in nodes:
+        paths = [nx.dijkstra_path(H, node, r, weight="weight")
+                 for r in roots]
+        path_lengths = np.array([path_length(H, p) for p in paths])
+        shortest = np.argmin(path_lengths)
+        mf.array()[paths[shortest]] = labels[shortest]
+
+    # Check that all nodes have been marked
+    if not np.all(mf.array()):
+        print("WARNING: Not all nodes have been marked, still zeros in mf.")
+
+    # Store computed data as XDMF (for easy reading/writing)
+    filename = os.path.join(output, "nearest_supply_nodes.xdmf")
+    with df.XDMFFile(mesh.mpi_comm(), filename) as xdmf:
+        xdmf.write(mf)
+        
+    return mf
+
+
+# Recursive function used to reduce full network to minimal set of
+# nodes and edges
+def add_branches(tree, a0, a, a_, Gr):
+    degree = tree.degree(a)
+
+    # Handle roots and leafs:
+    if degree == 1:
+        if a0 == a:
+            (_, b) = list(tree.edges(a))[0]
+            add_branches(tree, a0, b, a, Gr)
+        else:
+            Gr.add_edge(a0, a)
+            
+    # If we are on a path (degree == 2)
+    if degree == 2:
+        (e0, e1) = list(tree.edges(a))
+        # Continue down the new edge (not including a_)
+        _, b = e0 if a_ in e1 else e1
+        assert (a == _), "Error in path traversal"
+        add_branches(tree, a0, b, a, Gr)
+        
+    # Ok, at a bifurcation
+    if degree == 3:
+        Gr.add_edge(a0, a) 
+        # Get the other edges
+        (e1, e2) = [e for e in tree.edges(a) if not a_ in e]
+        assert (a == e1[0] and a == e2[0]), "Error in tree traversal"
+        add_branches(tree, a, e1[1], a, Gr)
+        add_branches(tree, a, e2[1], a, Gr)
+
+    # Ok, at a bifurcation with more than 4
+    if degree == 4:
+        Gr.add_edge(a0, a) 
+        # Get the other edges
+        print(tree.edges(a))
+        (e1, e2, e3) = [e for e in tree.edges(a) if not a_ in e]
+        assert (a == e1[0] and a == e2[0] and a == e3[0]), "Error in tree traversal"
+        add_branches(tree, a, e1[1], a, Gr)
+        add_branches(tree, a, e2[1], a, Gr)
+        add_branches(tree, a, e3[1], a, Gr)
+
+    if degree > 4:
+        raise Exception("degree > 4")
+
 def map_vasculature(fname, output):
 
     # Read network information from 'fname' -- argh, don't rescale it.
     print("Reading from %s" % fname)
     mesh, radii, roots = read_vtk_network(fname, rescale_mm2m=False)
+    print("Number of network nodes = ", mesh.num_vertices())
+    print("Number of network edges = ", mesh.num_cells())
     mesh.init()
 
     # Compute the length of each cell (lazy version)
@@ -19,80 +119,68 @@ def map_vasculature(fname, output):
     cell_lengths = df.assemble(1*v*df.dx(domain=mesh)).get_local()
     
     # Use networkx directed graph representation of mesh for
-    # convenience. Add the size of each cell as edge weights. (MER has
-    # not found an interface for doing this without adding each edge
-    # separately.)
+    # convenience. Add the size of each cell as edge weights.
     G = nx.DiGraph()
     for i, (n0, n1) in enumerate(mesh.cells()):
         G.add_edge(n0, n1, weight=cell_lengths[i])
-    
-    print("Number of network nodes = ", mesh.num_vertices())
-    print("Number of network edges = ", mesh.num_cells())
 
-    # FIXME: Identify the network supply nodes (Currently identified
-    # by a semi-visual inspection.)
+    # Identify the network supply nodes (Currently identified by a
+    # semi-visual inspection.)
     (r0, r1, r2) = [4094, 7220, 7974]
 
-    # Get all terminal nodes (degree == 1)
-    degree = np.array([d for (n, d) in G.degree()])
-    terminals = np.where(degree == 1)[0]
-    print(terminals)
-
-    # Easier to work with the undirected graph here.
+    # Easier to work with the undirected graph from here on.
     H = G.to_undirected()
-    
-    # Compute shortest path from each of the terminals to the supply nodes.
-    def path_length(G, path):
-        length = 0.0
-        for (i, v) in enumerate(path[:-1]):
-            v0 = path[i]
-            v1 = path[i+1]
-            length += G.get_edge_data(v0, v1)["weight"]
-        return length
 
-    # Create a vertex function that holds whether r0 (1), r1 (2) or r2 (3) is the
-    # closest for each terminal node 
+    # Compute (or read pre-computed) paths to supply nodes
+    #mf = compute_shortest_paths(mesh, H, (r0, r1, r2), output=output)
+    filename = os.path.join(output, "nearest_supply_nodes.xdmf")
     mf = df.MeshFunction("size_t", mesh, 0, 0)
+    with df.XDMFFile(mesh.mpi_comm(), filename) as xdmf:
+        xdmf.read(mf)
 
-    labels = {0: r0, 1: r1, 2: r2}
-
-    for t in terminals:
-        print("Computing nearest supply node for terminal node ", t)
-        paths = [nx.dijkstra_path(H, t, r, weight="weight") for r in (r0, r1, r2)]
-        path_lengths = np.array([path_length(H, p) for p in paths])
-        print("path_lengths = ", path_lengths)
-        shortest = np.argmin(path_lengths)
-        print("shortest = ", shortest)
-        mf.array()[paths[shortest]] = labels[shortest]
-        
-    df.File(os.path.join(output, "nearest_supply_node.pvd")) << mf
-
-    # In addition ... 
-    # Compute shortest path from each of the supply nodes (r0, r1, r2)
-    # to the other(s) via Dijkstra's algorithm. (Undirected graph to
-    # make sure that the path is found.)
-    p01 = nx.dijkstra_path(H, r0, r1)
-    p02 = nx.dijkstra_path(H, r0, r2)
-    p12 = nx.dijkstra_path(H, r1, r2)
-
-    # Find common node n0 that is in all three sets:
-    n0 = set(p01).intersection(set(p02)).intersection(set(p12)).pop()
-    print("Common node n0 = ", n0)
+    #mf = df.MeshFunction("size_t", mesh, 0, 0)
+    #mf.array()[3712] = 3712
+    #mf.array()[9697] = 9697
+    #file = df.File(os.path.join(output, "nodes_with_degree4.pvd"))
+    #file << mf
     
-    p0 = nx.dijkstra_path(H, r0, n0)
-    p1 = nx.dijkstra_path(H, r1, n0) 
-    p2 = nx.dijkstra_path(H, r2, n0)
-   
-    # Store paths
-    print("Storing shortest paths to %s/..." % output)
-    mf = df.MeshFunction("size_t", mesh, 0, 0)
-    mf.array()[p0] = r0
-    mf.array()[p1] = r1
-    mf.array()[p2] = r2
-    df.File(os.path.join(output, "shortest_path_between_supply_nodes.pvd")) << mf
+    # Extract subgraphs by taking all nodes marked by r0, r1 and r2
+    # separately
+    for r in (r0, r1, r2):
+        print("r = ", r)
+        # Extract subgraph tree defined by marking
+        subnodes = np.where(mf.array() == r)[0]
+        tree = H.subgraph(subnodes)
+
+        # Start reduced graph by adding bifurcations and terminals as nodes
+        Gr = nx.DiGraph()
+        bifurcations = [b for b in tree if tree.degree(b) >= 3]
+        terminals = [t for t in tree if tree.degree(t) == 1]
+        Gr.add_nodes_from(bifurcations)
+        Gr.add_nodes_from(terminals)
+
+        print("Gr (init) = ", Gr)
+        add_branches(tree, r, r, r, Gr)
+        print("Gr (final) = ", Gr)
 
 if __name__ == '__main__':
 
-    map_vasculature("../mesh/networks/arteries_smooth.vtk", "../mesh/networks/common_paths")
+    map_vasculature("../mesh/networks/arteries_smooth.vtk",
+                    "../mesh/networks/common_paths")
     #map_vasculature("../mesh/networks/venes_smooth.vtk")
 
+        
+    # # Iterate over edges in subtree (depth-first)
+    # a0 = r
+    # for e in nx.edge_dfs(tree, source=r):
+    #     (a, b) = e
+    #     print("e = ", e)
+    #     print("a.degree() = ", tree.degree(a))
+    #     print("b.degree() = ", tree.degree(b))
+    #     if tree.degree(b) == 3:
+    #         b0 = b
+    #         Gr.add_edge(a0, b0)
+    #         a0 = b0
+    #         exit()   
+            
+                
