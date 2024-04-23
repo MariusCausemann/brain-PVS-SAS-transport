6e8e66e08d998fc2feb8d1a33ef0f45b54e6fa9d
Rami Masri <ramimasri@Ramis-MacBook-Pro-2.local>
2024-04-23 17:37:47 +0200

    uhad_prod
    Former-commit-id: cf95b0e6eaf4eee909dcfea880668b8e772070d8

---
+++ scripts/uhat_prod.py
@@ -0,0 +1,67 @@
+from dolfin import *
+
+from solver import * 
+import os
+import numpy as np
+from xii import *
+from petsc4py import PETSc
+import sympy as sp
+
+
+# get mesh 
+sas = Mesh()
+with XDMFFile('mesh/volmesh/mesh.xdmf') as f:
+    f.read(sas)
+    gdim = sas.geometric_dimension()
+    vol_subdomains = MeshFunction('size_t', sas, gdim, 0)
+    f.read(vol_subdomains, 'label')
+    sas.scale(1e-3)  # scale from mm to m
+
+# pick the sas mesh 
+sas_outer = EmbeddedMesh(vol_subdomains, 1) 
+
+# Define function spaces for velocity and pressure
+cell = sas_outer.ufl_cell()  
+Velm = VectorElement('Lagrange', cell, 2)
+Qelm = FiniteElement('Lagrange', cell, 1) 
+V    = FunctionSpace(sas_outer, Velm)
+Q    = FunctionSpace(sas_outer, Qelm) 
+
+# read the sas flow velocity 
+vel_file = "results/csf_flow/mid_mesh/csf_v.xdmf"
+with XDMFFile(vel_file) as file:
+    velocity_sas = Function(V)
+    file.read_checkpoint(velocity_sas, "velocity")
+File('results/velocity_test_sas.pvd') << velocity_sas
+
+
+# read the arterial network and mesh 
+pvs_ratio_artery  = 2.0 
+artery, artery_radii, artery_roots = read_vtk_network("mesh/networks/arteries_smooth.vtk")
+artery_radii = as_P0_function(artery_radii)
+pvs_radii = Function(artery_radii.function_space())
+pvs_radii.vector().set_local(pvs_ratio_artery*artery_radii.vector().get_local())
+
+# define velocity average using Fenics_ii average functionality 
+disk_pvs  = Disk(radius = pvs_radii, degree =11)
+disk_artery  = Disk(radius = artery_radii, degree =11)
+velocity_sas_averaged = Average(velocity_sas, artery, disk_pvs) -  Average(velocity_sas, artery, disk_artery)
+
+Qa = FunctionSpace(artery, 'CG', 1)
+tau = TangentCurve(artery)
+
+#  project velocity_sas_averaged \cdot tangent vector onto the function space over the network 
+
+u = TrialFunction(Qa)
+v = TestFunction(Qa)  
+dx_a = Measure('dx', domain=artery)
+a  = inner(u,v)*dx_a  
+L  = inner(dot(velocity_sas_averaged,tau), v)*dx_a
+A,b = map(ii_assemble, (a,L)) 
+A,b = map(ii_convert, (A,b)) 
+pvs_flow = Function(Qa) 
+
+solve(A, pvs_flow.vector(), b) 
+
+with XDMFFile('results/pvs_flow/pvs_flow.xdmf') as xdmf:
+    xdmf.write_checkpoint(pvs_flow, "velocity")
