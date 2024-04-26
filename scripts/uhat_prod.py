c5f7426b0231118982af37009a8d07b9a7240723
Rami Masri <ramimasri@Ramis-MacBook-Pro-2.local>
2024-04-26 18:28:17 +0200

    some fixes
    Former-commit-id: 846c5e29caae18779f53dd9c2bead1e427aea4ec

--- scripts/uhat_prod.py
+++ scripts/uhat_prod.py
@@ -10,7 +10,7 @@ import sympy as sp
 
 # get mesh 
 sas = Mesh()
-with XDMFFile('mesh/volmesh/mesh.xdmf') as f:
+with XDMFFile('mesh/mid_mesh/volmesh/mesh.xdmf') as f:
     f.read(sas)
     gdim = sas.geometric_dimension()
     vol_subdomains = MeshFunction('size_t', sas, gdim, 0)
@@ -18,23 +18,27 @@ with XDMFFile('mesh/volmesh/mesh.xdmf') as f:
     sas.scale(1e-3)  # scale from mm to m
 
 # pick the sas mesh 
-sas_outer = EmbeddedMesh(vol_subdomains, 1) 
+#sas_outer = EmbeddedMesh(vol_subdomains, 1) 
 
 # Define function spaces for velocity and pressure
-cell = sas_outer.ufl_cell()  
-Velm = VectorElement('Lagrange', cell, 2)
-Qelm = FiniteElement('Lagrange', cell, 1) 
-V    = FunctionSpace(sas_outer, Velm)
-Q    = FunctionSpace(sas_outer, Qelm) 
+cell = sas.ufl_cell()  
+Velm = VectorElement('Lagrange', cell, 3)
+Qelm = FiniteElement('Lagrange', cell, 2) 
+V    = FunctionSpace(sas, Velm)
+Q    = FunctionSpace(sas, Qelm) 
 
 # read the sas flow velocity 
 vel_file = "results/csf_flow/mid_mesh/csf_v.xdmf"
 with XDMFFile(vel_file) as file:
-    velocity_sas = Function(V)
-    file.read_checkpoint(velocity_sas, "velocity")
+    velocity_sas_ = Function(V)
+    file.read_checkpoint(velocity_sas_, "velocity")
+
+VP1_elem = VectorElement('Lagrange', cell, 1)
+VP1 = FunctionSpace(sas, VP1_elem)
+velocity_sas = Function(VP1) 
+velocity_sas = interpolate(velocity_sas_ , VP1)
+
 File('results/velocity_test_sas.pvd') << velocity_sas
-
-
 # read the arterial network and mesh 
 pvs_ratio_artery  = 2.0 
 artery, artery_radii, artery_roots = read_vtk_network("mesh/networks/arteries_smooth.vtk")
@@ -65,3 +69,6 @@ solve(A, pvs_flow.vector(), b)
 
 with XDMFFile('results/pvs_flow/pvs_flow.xdmf') as xdmf:
     xdmf.write_checkpoint(pvs_flow, "velocity")
+
+
+File('results/pvs_flow/pvs_flow.pvd') << pvs_flow
