845baa6ea1f97a09318b0f6b5e8d5ae6deccd3cd
Marius Causemann <mariuscausemann@yahoo.de>
2024-05-22 09:53:19 +0200

    cleanup

--- scripts/uhat_prod.py
+++ scripts/uhat_prod.py
@@ -3,51 +3,27 @@ from solver import *
 import os
 import numpy as np
 from xii import *
-<<<<<<< HEAD
 from petsc4py import PETSc
 import sympy as sp
 import typer
 
-def compute_avg_flow(meshname : str):
-    # get mesh 
+def compute_avg_flow(csf_flow_model:  str):
+
     sas = Mesh()
-    with XDMFFile(f'mesh/{meshname}/volmesh/colors.xdmf') as f:
+    with XDMFFile(f'results/csf_flow/{csf_flow_model}/csf_v.xdmf') as f:
         f.read(sas)
-        gdim = sas.geometric_dimension()
-        sas_components = MeshFunction('size_t', sas, gdim, 0)
-        f.read(sas_components, 'sas_components')
-        sas.scale(1e-3)  # scale from mm to m
+        V = VectorFunctionSpace(sas, "CG", 3)
+        velocity_sas = Function(V)
+        f.read_checkpoint(velocity_sas, "velocity")
 
-    # pick the sas mesh 
-    #sas_outer = EmbeddedMesh(vol_subdomains, 1) 
-
-    # Define function spaces for velocity and pressure
-    cell = sas.ufl_cell()  
-    Velm = VectorElement('Lagrange', cell, 3)
-    Qelm = FiniteElement('Lagrange', cell, 2) 
-    V    = FunctionSpace(sas, Velm)
-    Q    = FunctionSpace(sas, Qelm) 
-
-    # read the sas flow velocity 
-    vel_file = f'results/csf_flow/{meshname}/csf_v.xdmf'
-
-    with XDMFFile(vel_file) as file:
-        velocity_sas_ = Function(V)
-        file.read_checkpoint(velocity_sas_, "velocity")
-
-    #VP1_elem = VectorElement('Lagrange', cell, 1)
-    #VP1 = FunctionSpace(sas, VP1_elem)
-    #velocity_sas = Function(VP1) 
-    #velocity_sas = interpolate(velocity_sas_ , VP1)
-
-    File(f'results/csf_flow/{meshname}/velocity_test_sas.pvd') << velocity_sas_ 
-    exit()
     # read the arterial network and mesh 
     pvs_ratio_artery  = 2.0 
-    artery, artery_radii, artery_roots = read_vtk_network("mesh/networks/arteries_smooth.vtk")
+    artery, artery_radii, artery_roots = read_vtk_network("mesh/networks/arteries_smooth.vtk", rescale_mm2m=False)
     artery_radii = as_P0_function(artery_radii)
+    artery_radii.set_allow_extrapolation(True)
     pvs_radii = Function(artery_radii.function_space())
     pvs_radii.vector().set_local(pvs_ratio_artery*artery_radii.vector().get_local())
+    pvs_radii.set_allow_extrapolation(True)
 
     # define velocity average using Fenics_ii average functionality 
     disk_pvs  = Disk(radius = pvs_radii, degree =11)
@@ -68,81 +44,14 @@ def compute_avg_flow(meshname : str):
     A,b = map(ii_convert, (A,b)) 
     pvs_flow = Function(Qa) 
 
-
     solve(A, pvs_flow.vector(), b) 
-=======
-
-# get mesh 
-sas = Mesh()
-with XDMFFile('mesh/T1/volmesh/mesh.xdmf') as f:
-    f.read(sas)
-    gdim = sas.geometric_dimension()
-    vol_subdomains = MeshFunction('size_t', sas, gdim, 0)
-    f.read(vol_subdomains, 'label')
->>>>>>> 105e21a8d96fd1c715be6e7281723c960a5ebcde
 
     pvs_flow_vec = project(pvs_flow*tau, VectorFunctionSpace(artery, "DG", 0))
 
-<<<<<<< HEAD
-    with XDMFFile(f'results/csf_flow/{meshname}/pvs_flow.xdmf') as xdmf:
+    with XDMFFile(f'results/csf_flow/{csf_flow_model}/pvs_flow.xdmf') as xdmf:
         xdmf.write_checkpoint(pvs_flow_vec, "velocity")
-=======
-# Define function spaces for velocity and pressure
-cell = sas.ufl_cell()  
-Velm = VectorElement('Lagrange', cell, 3)
-Qelm = FiniteElement('Lagrange', cell, 2) 
-V    = FunctionSpace(sas, Velm)
-Q    = FunctionSpace(sas, Qelm) 
 
-# read the sas flow velocity 
-vel_file = "results/csf_flow/T1/csf_v.xdmf"
-with XDMFFile(vel_file) as file:
-    velocity_sas_ = Function(V)
-    file.read_checkpoint(velocity_sas_, "velocity")
+    File(f'results/csf_flow/{csf_flow_model}/pvs_flow.pvd') << pvs_flow_vec
 
-VP1_elem = VectorElement('Lagrange', cell, 1)
-VP1 = FunctionSpace(sas, VP1_elem)
-velocity_sas = Function(VP1) 
-velocity_sas = interpolate(velocity_sas_ , VP1)
-
-# read the arterial network and mesh 
-pvs_ratio_artery  = 2.0 
-artery, artery_radii, artery_roots = read_vtk_network("mesh/networks/arteries_smooth.vtk", rescale_mm2m=False)
-artery_radii = as_P0_function(artery_radii)
-pvs_radii = Function(artery_radii.function_space())
-pvs_radii.vector().set_local(pvs_ratio_artery*artery_radii.vector().get_local())
-
-# define velocity average using Fenics_ii average functionality 
-disk_pvs  = Disk(radius = pvs_radii, degree =11)
-disk_artery  = Disk(radius = artery_radii, degree =11)
-velocity_sas_averaged = Average(velocity_sas, artery, disk_pvs) -  Average(velocity_sas, artery, disk_artery)
-
-Qa = FunctionSpace(artery, 'CG', 1)
-tau = TangentCurve(artery)
-
-#  project velocity_sas_averaged \cdot tangent vector onto the function space over the network 
-
-u = TrialFunction(Qa)
-v = TestFunction(Qa)  
-dx_a = Measure('dx', domain=artery)
-a  = inner(u,v)*dx_a  
-L  = inner(dot(velocity_sas_averaged,tau), v)*dx_a
-A,b = map(ii_assemble, (a,L)) 
-A,b = map(ii_convert, (A,b)) 
-pvs_flow = Function(Qa) 
->>>>>>> 105e21a8d96fd1c715be6e7281723c960a5ebcde
-
-
-    File(f'results/csf_flow/{meshname}/pvs_flow.pvd') << pvs_flow_vec
-
-<<<<<<< HEAD
 if __name__ == "__main__":
     typer.run(compute_avg_flow)
-=======
-pvs_flow_vec = project(pvs_flow*tau, VectorFunctionSpace(artery, "DG", 0))
-
-with XDMFFile('results/pvs_flow/uhat_prod.xdmf') as xdmf:
-    xdmf.write_checkpoint(pvs_flow_vec, "velocity")
-
-File('results/pvs_flow/uhat_prod.pvd') << pvs_flow_vec
->>>>>>> 105e21a8d96fd1c715be6e7281723c960a5ebcde
