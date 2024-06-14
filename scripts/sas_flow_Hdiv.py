3027f97068e948a804192f87702e262378317ca0
Marius Causemann <mariuscausemann@yahoo.de>
2024-06-14 09:46:18 +0200

    update inflow

--- scripts/sas_flow_Hdiv.py
+++ scripts/sas_flow_Hdiv.py
@@ -16,55 +16,6 @@ def directsolve(a, L, bcs, a_prec, W):
     solve(a==L, wh, bcs=bcs, solver_parameters={"linear_solver":"mumps"})
     return wh
 
-def iterativesolve(a, L, bcs, a_prec, W):
-    A, b = assemble_system(a, L, bcs)
-    B, _ = assemble_system(a_prec, L, bcs) 
-
-    ns = Function(W)    # W is mixed FS
-    nullspace_basis = ns.vector().copy()
-    W.sub(1).dofmap().set(nullspace_basis, 1.0)
-    nullspace_basis.apply('insert')
-    nullspace = VectorSpaceBasis([nullspace_basis])
-    nullspace.orthonormalize()
-
-    as_backend_type(A).set_nullspace(nullspace)
-    wh = Function(W)
-    solver = PETScKrylovSolver()
-    solver.set_operators(A, B)
-
-    ksp = solver.ksp()
-    opts = PETSc.Options()
-    opts.setValue('ksp_type', 'minres')
-    opts.setValue('ksp_minres_qlp', 1)
-    opts.setValue('ksp_rtol', 1E-5)                
-    opts.setValue('ksp_view_pre', None)
-    opts.setValue('ksp_monitor_true_residual', None)                
-    opts.setValue('ksp_converged_reason', None)
-    # Specify that the preconditioner is block diagonal and customize
-    # inverses of individual blocks
-    opts.setValue('fieldsplit_0_ksp_type', 'preonly')
-    opts.setValue('fieldsplit_0_pc_type', 'lu')
-    opts.setValue('fieldsplit_0_pc_factor_mat_solver_type', 'mumps')
-    opts.setValue('fieldsplit_1_ksp_type', 'preonly')
-    opts.setValue('fieldsplit_1_pc_type', 'lu')
-    opts.setValue('fieldsplit_1_pc_factor_mat_solver_type', 'mumps')
-    pc = ksp.getPC()
-    pc.setType(PETSc.PC.Type.FIELDSPLIT)
-    is_V = PETSc.IS().createGeneral(W.sub(0).dofmap().dofs())
-    is_Q = PETSc.IS().createGeneral(W.sub(1).dofmap().dofs())
-    pc.setFieldSplitIS(('0', is_V), ('1', is_Q))
-    pc.setFieldSplitType(PETSc.PC.CompositeType.ADDITIVE) 
-    ksp.setUp()
-    subksps = pc.getFieldSplitSubKSP()
-    A0, B0 = subksps[0].getOperators()
-    gdim = W.mesh().geometry().dim()
-    A0.setBlockSize(gdim)
-    B0.setBlockSize(gdim)
-    pc.setFromOptions()
-    ksp.setFromOptions()
-    niters = solver.solve(wh.vector(), b)
-    return wh
-
 PETScOptions.set("mat_mumps_icntl_4", 3)  # mumps verbosity
 #PETScOptions.set("mat_mumps_icntl_24", 1)  # null pivot detection
 PETScOptions.set("mat_mumps_icntl_35", 1)  # BLR feature
@@ -86,50 +37,25 @@ EFFLUX_ID = 1
 NO_SLIP_ID = 2
 LV_INTERF_ID = 3
 SPINAL_OUTLET_IT = 4
+CSF_INTERF_ID = 5
 
-def map_on_global(uh, parentmesh, eps_digits=16):
+def map_on_global(uh, parentmesh, eps_digits=10):
     # map the solution back on the whole domain
     el = uh.function_space().ufl_element()
     V = uh.function_space()
-    eldim = 1 if len(uh.ufl_shape)==0 else uh.ufl_shape[0]
+    if el.family()=="Lagrange":
+        bdim = 1 if len(uh.ufl_shape)==0 else uh.ufl_shape[0]
+    if el.family()=="Brezzi-Douglas-Marini":
+        bdim = 1
+    if el.family()=='Discontinuous Lagrange' and el.degree()==0:
+        bdim = 1
     V_glob   = FunctionSpace(parentmesh, el)
-    c_coords = np.round(V.tabulate_dof_coordinates()[::eldim,:], eps_digits)
-    p_coords = np.round(V_glob.tabulate_dof_coordinates()[::eldim,:], eps_digits)
+    c_coords = np.round(V.tabulate_dof_coordinates()[::bdim,:], eps_digits)
+    p_coords = np.round(V_glob.tabulate_dof_coordinates()[::bdim,:], eps_digits)
     idxmap = npi.indices(p_coords, c_coords, axis=0)
     uh_global = Function(V_glob)
-    for i in range(eldim):
-        uh_global.vector()[idxmap*eldim + i] = uh.vector()[i::eldim]
-    return uh_global
-
-def map_on_global2(uh, parentmesh, eps_digits=12):
-    # map the solution back on the whole domain
-    el = uh.function_space().ufl_element()
-    V = uh.function_space()
-    eldim = 1 if len(uh.ufl_shape)==0 else uh.ufl_shape[0]
-    V_glob = FunctionSpace(parentmesh, el)
-    
-    # Increase precision for matching coordinates
-    c_coords = np.round(V.tabulate_dof_coordinates()[::eldim, :], eps_digits)
-    p_coords = np.round(V_glob.tabulate_dof_coordinates()[::eldim, :], eps_digits)
-    
-    try:
-        idxmap = npi.indices(p_coords, c_coords, axis=0)
-    except KeyError as e:
-        # Handle missing coordinates
-        idxmap = npi.indices(p_coords, c_coords, axis=0, missing='mask')
-        if np.ma.is_masked(idxmap):
-            missing_indices = np.where(np.ma.getmask(idxmap))[0]
-            print(f"Some local coordinates are not present in the global coordinates. Missing indices: {missing_indices} length: {len(missing_indices)}, {len(c_coords)}")
-            # Optionally, handle missing indices here
-            idxmap = np.ma.filled(idxmap, fill_value= 0.0)  # or any other way to handle missing points
-
-    uh_global = Function(V_glob)
-    for i in range(eldim):
-        if np.any(idxmap < 0):
-            print(f"Warning: Some coordinates were not mapped correctly. Skipping these indices.")
-        valid_idx = idxmap >= 0
-        uh_global.vector()[idxmap[valid_idx] * eldim + i] = uh.vector()[i::eldim][valid_idx]
-
+    for i in range(bdim):
+        uh_global.vector()[idxmap*bdim + i] = uh.vector()[i::bdim]
     return uh_global
 
 def get_normal_func(mesh):
@@ -148,7 +74,7 @@ def get_normal_func(mesh):
     return nh
 
 
-def compute_sas_flow(configfile : str, elm:str = 'cr'):
+def compute_sas_flow(configfile : str, elm:str = 'BDM'):
 
     config = read_config(configfile)
     modelname = Path(configfile).stem
@@ -178,10 +104,15 @@ def compute_sas_flow(configfile : str, elm:str = 'cr'):
     mark_internal_interface(sas, label, boundary_markers, LV_INTERF_ID,
                             doms=[LVID, PARID])
     
+    mark_internal_interface(sas, label, boundary_markers, CSF_INTERF_ID,
+                            doms=[CSFID, PARID])
+    
     # translate markers to the sas outer mesh 
     boundary_markers_outer = MeshFunction("size_t", sas_outer, sas_outer.topology().dim() - 1, 0)
     DomainBoundary().mark(boundary_markers_outer, NO_SLIP_ID)
-    sas_outer.translate_markers(boundary_markers, (EFFLUX_ID, LV_INTERF_ID), marker_f=boundary_markers_outer)
+    sas_outer.translate_markers(boundary_markers,
+                                (EFFLUX_ID, LV_INTERF_ID, CSF_INTERF_ID),
+                                marker_f=boundary_markers_outer)
 
     spinal_outlet = CompiledSubDomain("on_boundary && x[2] < zmin + eps",
                                        zmin=sas.coordinates()[:,2].min(), eps=1e-3)
@@ -193,18 +124,9 @@ def compute_sas_flow(configfile : str, elm:str = 'cr'):
 
     # Define function spaces for velocity and pressure
     cell = sas_outer.ufl_cell()  
-    if elm == "BDM":
-        Velm = FiniteElement('BDM', cell, 1)
-        Qelm = FiniteElement('Discontinuous Lagrange', cell, 0) 
-    elif elm == "cr":
-        Velm = VectorElement('Crouzeix-Raviart', cell, 1)
-        Qelm = FiniteElement('Discontinuous Lagrange', cell, 0)
-    else: 
-        Velm = VectorElement('Lagrange', cell, 3)
-        Qelm = FiniteElement('Lagrange', cell, 2) 
-    
+    Velm = FiniteElement('BDM', cell, 1)
+    Qelm = FiniteElement('Discontinuous Lagrange', cell, 0) 
 
-    Q = FunctionSpace(sas_outer, Qelm)
     W = FunctionSpace(sas_outer, Velm * Qelm)
 
     u , p = TrialFunctions(W) 
@@ -216,10 +138,9 @@ def compute_sas_flow(configfile : str, elm:str = 'cr'):
     f = Constant([0]*gdim)
 
     LV_surface_area = assemble(1*ds(LV_INTERF_ID))
-    total_production = config["production_rate"]
-    g = total_production / LV_surface_area
-    g_L_per_day = 1e3 *(60*60*24) * assemble(g*ds(LV_INTERF_ID))
-    print(f"production rate: {g_L_per_day} L/day")
+    PAR_surface_area = assemble(1*ds(CSF_INTERF_ID))
+    g_LV = config["LV_inflow_rate"] / LV_surface_area
+    g_par = config["tissue_inflow_rate"] / PAR_surface_area
 
     a = (inner(2*mu*sym(grad(u)), sym(grad(v)))*dx - inner(p, div(v))*dx
             -inner(q, div(u))*dx + inner(R*dot(u,n), dot(v,n))*ds(EFFLUX_ID)) 
@@ -253,26 +174,32 @@ def compute_sas_flow(configfile : str, elm:str = 'cr'):
 
     hF = CellDiameter(sas_outer)
 
-    if elm == 'cr':
-        a += 2*(mu/avg(hF))*inner(jump(u), jump(v))*dS
-    elif elm == 'BDM':
-        a += Stabilization(sas_outer, u,v, mu, penalty)
+    a += Stabilization(sas_outer, u,v, mu, penalty)
 
     L = inner(f, v)*dx
+    LV_inflow = get_normal_func(sas_outer)
+    LV_inflow.vector()[:] *= -g_LV
+    PAR_inflow = get_normal_func(sas_outer)
+    PAR_inflow.vector()[:] *= -g_par
+    bcs = [
+           DirichletBC(W.sub(0), Constant((0, 0, 0)), ds.subdomain_data(), NO_SLIP_ID),
+           DirichletBC(W.sub(0), LV_inflow, ds.subdomain_data(), LV_INTERF_ID),
+           DirichletBC(W.sub(0), PAR_inflow, ds.subdomain_data(), CSF_INTERF_ID)
+           ]
+    
+    # add no-slip BC
+    for bid in [NO_SLIP_ID, LV_INTERF_ID, CSF_INTERF_ID]:
+        a += -inner(dot(2*mu*D(v), n), Tangent(u, n))*ds(bid) \
+            - inner(dot(2*mu*D(u), n), Tangent(v, n))*ds(bid) \
+            + 2*mu*(penalty/hF)*inner(Tangent(u, n), Tangent(v, n))*ds(bid)
+        
+    
 
-    CG2 = VectorFunctionSpace(sas_outer, "CG", 2)
-    inflow = get_normal_func(sas_outer)
-    inflow.vector()[:] *= -g
-    bcs = [DirichletBC(W.sub(0), Constant((0, 0, 0)), ds.subdomain_data(), NO_SLIP_ID),
-           DirichletBC(W.sub(0), inflow, ds.subdomain_data(), LV_INTERF_ID)]
-    if elm == 'BDM':
-         a += -inner(dot(2*mu*D(v), n), Tangent(u, n))*ds(NO_SLIP_ID) \
-            + 2*mu*(penalty/hF)*inner(Tangent(u, n), Tangent(v, n))*ds(NO_SLIP_ID) 
     if config["spinal_outflow_bc"] == "noslip":
         bcs += [DirichletBC(W.sub(0), Constant((0, 0, 0)), ds.subdomain_data(), SPINAL_OUTLET_IT)]
-        if elm == 'BDM':
-            a += -inner(dot(2*mu*D(v), n), Tangent(u, n))*ds(SPINAL_OUTLET_IT) \
-                 + 2*mu*(penalty/hF)*inner(Tangent(u, n), Tangent(v, n))*ds(SPINAL_OUTLET_IT)  
+        a += -inner(dot(2*mu*D(v), n), Tangent(u, n))*ds(SPINAL_OUTLET_IT) \
+            - inner(dot(2*mu*D(u), n), Tangent(v, n))*ds(SPINAL_OUTLET_IT) \
+                + 2*mu*(penalty/hF)*inner(Tangent(u, n), Tangent(v, n))*ds(SPINAL_OUTLET_IT)  
     elif config["spinal_outflow_bc"] == "zeroneumann":
         pass
     else:
@@ -281,47 +208,50 @@ def compute_sas_flow(configfile : str, elm:str = 'cr'):
     a_prec = (inner(2*mu*grad(u), grad(v))*dx + inner(R*dot(u,n), dot(v,n))*ds(EFFLUX_ID)
                     + (1/mu)*inner(p, q)*dx) 
 
-    #wh = iterativesolve(a, L, bcs, a_prec, W)
     wh = directsolve(a, L, bcs, a_prec, W)
     uh, ph = wh.split(deepcopy=True)[:]
-    #assert np.isclose(assemble(inner(uh,-n)*ds(LV_INTERF_ID)), total_production, rtol=0.03)
     
-    # project to CG2 and write for visualization
-    uh2 = project(uh, CG2, solver_type="cg", preconditioner_type="hypre_amg")
-    with XDMFFile(f'{results_dir}/csf_vis_v.xdmf') as xdmf:
-        xdmf.write_checkpoint(uh2, "velocity")
-    with XDMFFile(f'{results_dir}/csf_vis_p.xdmf') as xdmf:
+    uhdg = interpolate(uh, VectorFunctionSpace(sas_outer, "DG", 1))
+    assert np.isclose(assemble(div(uhdg)*div(uhdg)*dx), 0)
+    with XDMFFile(f'{results_dir}/csf_vis_v_bdm.xdmf') as xdmf:
+        xdmf.write_checkpoint(uhdg, "velocity")
+    with XDMFFile(f'{results_dir}/csf_vis_p_bdm.xdmf') as xdmf:
         xdmf.write_checkpoint(ph, "pressure")
     
-    from IPython import embed  
-    embed()
-    uh_global = map_on_global(uh, sas)
+    #uh_global = map_on_global(uh, sas)
+    uh.set_allow_extrapolation(True)
+    uh_global = interpolate(uh, FunctionSpace(sas, "BDM", 1))
     dxglob = Measure("dx", sas, subdomain_data=label)
 
-    assert np.isclose(assemble(inner(uh_global, uh_global)*dxglob(PARID)), 0)
-    assert np.isclose(assemble(inner(uh_global, uh_global)*dxglob(CSFNOFLOWID)), 0)
-    assert np.isclose(assemble(inner(uh, uh)*dx), assemble(inner(uh_global, uh_global)*dxglob))
+    #assert np.isclose(assemble(inner(uh_global, uh_global)*dxglob(PARID)), 0)
 
-    divu_global = assemble(div(uh_global)*div(uh_global)*dxglob)
+    divu_global_csf = assemble(div(uh_global)*div(uh_global)*dxglob(CSFID)) \
+                    + assemble(div(uh_global)*div(uh_global)*dxglob(LVID)) \
+                    + assemble(div(uh_global)*div(uh_global)*dxglob(V34ID))
+
     divu = assemble(div(uh)*div(uh)*dx)
-    print(divu) 
-    print(divu_global)
-    #  assert np.isclose(divu, divu_global, rtol=0.2)
 
-    with XDMFFile(f'{results_dir}/csf_v.xdmf') as xdmf:
+    assert np.isclose(divu, 0)
+
+    csf_indicator = MeshFunction('size_t', sas, gdim, 0)
+    csf_indicator.array()[np.isin(label.array(), [CSFID, LVID, V34ID])] = 1
+    uh_global_dg = project(as_P0_function(csf_indicator) * uh_global,
+                                VectorFunctionSpace(sas, "DG", 1), solver_type="mumps")
+
+    with XDMFFile(f'{results_dir}/csf_v_bdm.xdmf') as xdmf:
         xdmf.write(sas)
         xdmf.write_checkpoint(uh_global, "velocity", append=True)
 
-    with XDMFFile(f'{results_dir}/csf_p.xdmf') as xdmf:
-        xdmf.write_checkpoint(ph, "pressure")
+    with XDMFFile(f'{results_dir}/csf_v_bdm_glob.xdmf') as xdmf:
+        xdmf.write_checkpoint(uh_global_dg, "velocity")
 
-    umag = project(sqrt(inner(uh, uh)), FunctionSpace(sas_outer, "CG", 3),
-                   solver_type="cg", preconditioner_type="hypre_amg")
-    umean = assemble(umag*dx) / assemble(1*dx(domain=sas_outer))
+    with XDMFFile(f'{results_dir}/csf_p_bdm.xdmf') as xdmf:
+        xdmf.write_checkpoint(map_on_global(ph, sas), "pressure")
+
+    assert np.isclose(divu, divu_global_csf)
+    #assert np.isclose(assemble(inner(uh_global, uh_global)*dxglob(CSFNOFLOWID)), 0)
 
     print(f"div u = {divu}")
-    print(f"u max = {umag.vector().max()}")
-    print(f"u umean = {umean}")
 
 if __name__ == "__main__":
     typer.run(compute_sas_flow)
