d54ab4167e48dc6f106b1576ef3336c3e7593a07
Marius Causemann <mariuscausemann@yahoo.de>
2024-06-14 09:50:35 +0200

    attempt for BDM, not working :_/
    Former-commit-id: fce97edf3ab5540d61cef39bf670364aa9afb373 [formerly f965e643e46ae2272cc4219f282521d27931a230]
    Former-commit-id: 62d246e48fd4c58a4508444ee25d7b5c0cc86799

--- scripts/test_map_on_global.py
+++ scripts/test_map_on_global.py
@@ -2,6 +2,7 @@ from fenics import *
 from xii import *
 import numpy_indexed as npi
 import numpy as np
+from IPython import embed
 
 CSFID = 1
 PARID = 2
@@ -9,20 +10,27 @@ LVID = 3
 V34ID = 4
 CSFNOFLOWID = 5
 
-def map_on_global(uh, parentmesh, eps_digits=10):
+def map_on_global(uh, parentmesh, eps_digits=12):
     # map the solution back on the whole domain
     el = uh.function_space().ufl_element()
     V = uh.function_space()
-    eldim = 1 if len(uh.ufl_shape)==0 else uh.ufl_shape[0]
-    V_glob = FunctionSpace(parentmesh, el)
-    c_coords = np.round(V.tabulate_dof_coordinates()[::eldim,:], eps_digits)
-    p_coords = np.round(V_glob.tabulate_dof_coordinates()[::eldim,:], eps_digits)
+    if el.family()=="Lagrange":
+        bdim = 1 if len(uh.ufl_shape)==0 else uh.ufl_shape[0]
+    if el.family()=="Brezzi-Douglas-Marini":
+        bdim = 1
+    if el.family()=='Discontinuous Lagrange' and el.degree()==0:
+        bdim = 1
+    V_glob   = FunctionSpace(parentmesh, el)
+    c_coords = np.round(V.tabulate_dof_coordinates()[::bdim,:], eps_digits)
+    p_coords = np.round(V_glob.tabulate_dof_coordinates()[::bdim,:], eps_digits)
     idxmap = npi.indices(p_coords, c_coords, axis=0)
     uh_global = Function(V_glob)
-    for i in range(eldim):
-        uh_global.vector()[idxmap*eldim + i] = uh.vector()[i::eldim]
-    np.isclose(uh.vector().sum(), uh_global.vector().sum())
+    for i in range(bdim):
+        uh_global.vector()[idxmap*bdim + i] = uh.vector()[i::bdim]
+    assert np.isclose(uh.vector().sum(), uh_global.vector().sum())
     return uh_global
+
+"""
 
 mesh = Mesh()
 with XDMFFile(f'mesh/T1/volmesh/mesh.xdmf') as f:
@@ -32,19 +40,29 @@ with XDMFFile(f'mesh/T1/volmesh/mesh.xdmf') as f:
     f.read(label, 'label')
 
 emb = EmbeddedMesh(label, [CSFID,LVID,V34ID]) 
+"""
+mesh = UnitSquareMesh(4,4)
+left = CompiledSubDomain("x[0] <= 0.5")
+mf = MeshFunction("size_t", mesh, 2, 0)
+left.mark(mf, 1)
+emb = EmbeddedMesh(mf, 1) 
 
-CG2emb = VectorFunctionSpace(emb, "CG", 2)
-uh = interpolate(Expression(("1", "0", "0"), degree=2), CG2emb)
-bc = DirichletBC(CG2emb, Constant([0,0,0]), "on_boundary")
-bc.apply(uh.vector())
+gdim = mesh.geometric_dimension()
+CG2emb = FunctionSpace(emb, "BDM", 1)
+uh = interpolate(Expression(("sin(x[0]*pi)", "sin(x[1]*pi)"),degree=1), CG2emb)
+bc = DirichletBC(CG2emb, Constant([0]*gdim), "on_boundary")
+#bc.apply(uh.vector())
 
 uh_global = map_on_global(uh, mesh)
 
 print(assemble(div(uh)*div(uh)*dx))
 print(assemble(div(uh_global)*div(uh_global)*dx))
 
+uhdg = interpolate(uh, VectorFunctionSpace(emb, "DG", 1))
 with XDMFFile("uh.xdmf") as file:
-    file.write_checkpoint(uh, "uh")
+    file.write_checkpoint(uhdg, "uh")
 
+uh.set_allow_extrapolation(True)
+uh_global_dg = interpolate(uh_global, VectorFunctionSpace(mesh, "DG", 1))
 with XDMFFile("uh_gobal.xdmf") as file:
-    file.write_checkpoint(uh_global, "uh_global")
+    file.write_checkpoint(uh_global_dg, "uh_global")
