a6cc7f087d999f792ba54914097f27ad4a20cc71
Marius Causemann <mariuscausemann@yahoo.de>
2024-09-02 16:31:04 +0200

    rudimentary L2 errr
    Former-commit-id: 75a19b44956e262bf12aa5376a0efd54f4d2b214 [formerly 75a19b44956e262bf12aa5376a0efd54f4d2b214 [formerly 5eca0c670be34c5021333f2f26b75a5aed388ebf]]
    Former-commit-id: b6096f16c728dd99805d0903e78c1373fd5b6531
    Former-commit-id: 6d76865811443fdd865cf578bcc4bb0ccb333c8c

--- scripts/compare_solutions.py
+++ scripts/compare_solutions.py
@@ -1,11 +1,31 @@
 from fenics import *
 
-file1 = "results/csf_flow/sas_flow/csf_v.xdmf"
+filefine = "results/csf_flow/sas_flow_HighRes/csf_v.xdmf"
+filecoarse = "results/csf_flow/sas_flow/csf_v.xdmf"
 space = "DG"
 
 mesh1 = Mesh()
-with XDMFFile(file1) as f:
+with XDMFFile(filefine) as f:
     f.read(mesh1)
-    V = VectorFunctionSpace(mesh1, space, 1)
-    velocity_sas = Function(V)
-    f.read_checkpoint(velocity_sas, "velocity")
+    V1 = VectorFunctionSpace(mesh1, space, 1)
+    sol_fine = Function(V1)
+    f.read_checkpoint(sol_fine, "velocity")
+
+mesh2 = Mesh()
+with XDMFFile(filecoarse) as f:
+    f.read(mesh2)
+    V2 = VectorFunctionSpace(mesh2, space, 1)
+    sol_coarse = Function(V2)
+    f.read_checkpoint(sol_coarse, "velocity")
+
+sol_coarse.set_allow_extrapolation(True)
+coarse_on_fine = project(sol_coarse, V1, solver_type="mumps")
+
+diff = Function(V1)
+diff.vector()[:] = sol_fine.vector()[:] - coarse_on_fine.vector()[:]
+
+L2fine = norm(sol_fine)
+L2coarse =  norm(coarse_on_fine)
+L2diff = norm(diff)
+
+relerror = L2diff / L2fine
