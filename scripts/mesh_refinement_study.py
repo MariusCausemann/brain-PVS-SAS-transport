8ff9c4a29db3b7a1a2949d5c9458665e44f99d76
Marius Causemann <mariusca@simula.no>
2024-09-07 15:31:05 +0200

    add undersoots and refine table

--- scripts/mesh_refinement_study.py
+++ scripts/mesh_refinement_study.py
@@ -1,8 +1,14 @@
 import yaml
 from plotting_utils import read_config
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
 
-meshes = ["LowRes", ] #, "standard", "HighRes"]
+os.makedirs("plots/mesh_refinement/", exist_ok=True)
+
+meshes = ["LowRes", "standard", "HighRes"]
 csf_flow_model = {"LowRes":"sas_flow_LowRes",
                   "standard":"sas_flow",
                   "HighRes":"sas_flow_HighRes"}
@@ -19,6 +25,9 @@ for m in meshes:
     md.update(read_config(f"results/csf_flow/{csfm}/metrics.yml"))
     # get PVS flow stats
     md.update(read_config(f"results/csf_flow/{csfm}/pvs_metrics.yml"))
+    # get cardiac PVS flow stats
+    cardiac_csf = read_config(f"results/csf_flow/cardiac_{csfm}/metrics.yml")
+    md.update({f"cardiac_{k}":v for k,v in cardiac_csf.items()})
     # get mean concentrations
     tm = transport_model[m]
     md.update(read_config(f"results/{tm}/mean_concentrations.yml"))
@@ -32,3 +41,56 @@ for m in meshes:
                                                np.array(trmetrics[f"{dom}_max"]) ) },
                    )
     results[m] = md
+
+df = pd.DataFrame(results)
+dfstr = df.applymap('{:,.3g}'.format)
+fig, ax = plt.subplots(figsize=(6,22))
+ax.axis('tight')
+ax.axis('off')
+the_table = ax.table(cellText=dfstr.values,
+                        rowLabels=dfstr.index,
+                        colLabels=dfstr.columns,
+                        loc="center")
+fig.tight_layout()
+fig.savefig("plots/mesh_refinement/table.png")
+
+
+for m in meshes:
+    df[f"{m}_rel"] = df[m] / df["standard"]
+
+dfrel = df[[f"{m}_rel" for m in meshes]]
+
+plt.figure()
+#dfrel = dfrel[~dfrel.index.isin(["ven_min", "rel_undershot_ven"])]
+ax = dfrel.plot(kind="bar", figsize=(40,4), ylim=(0, 5))
+pps = ax.containers[1]
+for i, p, val in zip(range(len(pps)), pps, df["standard"].values):
+   height = p.get_height()
+   ax.annotate('{0:6.2g}'.format(val),
+      xy=(p.get_x() + p.get_width() / 2, height),
+      xytext=(0, 3 + 2*(i%2)), # 3 points vertical offset
+      textcoords="offset points",
+      fontsize=6,
+      ha='center', va='bottom')
+
+plt.tight_layout()
+plt.savefig("plots/mesh_refinement/mesh_bars.png")
+
+# investigate undershoots
+plt.figure()
+
+for m in meshes:
+    tm = transport_model[m]
+    tmconfig = read_config(f"configfiles/{tm}.yml")
+    trmetrics = read_config(f"results/{tm}/{tm}_metrics.yml")
+    csf_max = trmetrics["csf_max"]
+    csf_min = trmetrics["csf_min"]
+    times = np.arange(0, tmconfig["T"], tmconfig["dt"]) / (60*60)
+    plt.plot(times, csf_max, label=f"max {m}")
+    plt.plot(times, csf_min, label=f"min {m}")
+plt.xlabel("time (h)")
+plt.ylabel("tracer concentration (mol/L)")
+plt.legend()
+plt.savefig(f"plots/mesh_refinement/undershoots.png")
+
+
