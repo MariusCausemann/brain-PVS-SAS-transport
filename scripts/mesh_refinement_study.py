c7ee13135db5936e922ab96ed9922ea38c42b0e7
Marius Causemann <mariusca@simula.no>
2024-09-16 11:55:01 +0200

    add time refinement case
    Former-commit-id: 368e621954ac4870b84eb8a61dac35622983c36c [formerly 0643d41b866f464e1cd0f973b2c6518f39ac4bdc]
    Former-commit-id: 2f4be90df90979472adae7a86004c81e6b0f6a78

--- scripts/mesh_refinement_study.py
+++ scripts/mesh_refinement_study.py
@@ -5,92 +5,108 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
+from pathlib import Path
 
-os.makedirs("plots/mesh_refinement/", exist_ok=True)
-
-meshes = ["LowRes", "standard", "HighRes"]
 csf_flow_model = {"LowRes":"sas_flow_LowRes",
-                  "standard":"sas_flow",
-                  "HighRes":"sas_flow_HighRes"}
+                "standard":"sas_flow",
+                "HighRes":"sas_flow_HighRes",
+                "large dt":"sas_flow",
+                "small dt":"sas_flow"}
 transport_model = {"LowRes":"modelALowRes",
-                  "standard":"modelA",
-                  "HighRes":"modelAHighRes"}
-results = dict()
-for m in meshes:
-    md = dict()
-    # get mesh stats
-    md.update(read_config(f"mesh/{m}/{m}.yml"))
-    # get CSF flow stats
-    csfm = csf_flow_model[m]
-    md.update(read_config(f"results/csf_flow/{csfm}/metrics.yml"))
-    # get PVS flow stats
-    md.update(read_config(f"results/csf_flow/{csfm}/pvs_metrics.yml"))
-    # get cardiac PVS flow stats
-    cardiac_csf = read_config(f"results/csf_flow/cardiac_{csfm}/metrics.yml")
-    md.update({f"cardiac_{k}":v for k,v in cardiac_csf.items()})
-    # get mean concentrations
-    tm = transport_model[m]
-    md.update(read_config(f"results/{tm}/mean_concentrations.yml"))
-    trmetrics = read_config(f"results/{tm}/{tm}_metrics.yml")
-    md.update({"Rmax":trmetrics["R_max"],"Rmean":trmetrics["R_mean"],
-               "Rmin":trmetrics["R_min"] })
-    for dom in ["csf", "art", "ven"]:
-        md.update({f"{dom}_max": max(trmetrics[f"{dom}_max"]),
-                   f"{dom}_min": max(trmetrics[f"{dom}_min"]),
-                   f"rel_undershot_{dom}": min(np.array(trmetrics[f"{dom}_min"]) /
-                                               np.array(trmetrics[f"{dom}_max"]) ) },
-                   )
-    results[m] = md
+                "standard":"modelA",
+                "HighRes":"modelAHighRes",
+                "large dt":"modelAlargedt",
+                "small dt":"modelAsmalldt"}
 
-df = pd.DataFrame(results)
-dfstr = df.applymap('{:,.3g}'.format)
-fig, ax = plt.subplots(figsize=(6,22))
-ax.axis('tight')
-ax.axis('off')
-the_table = ax.table(cellText=dfstr.values,
-                        rowLabels=dfstr.index,
-                        colLabels=dfstr.columns,
-                        loc="center")
-fig.tight_layout()
-fig.savefig("plots/mesh_refinement/table.png")
+mesh_refinement_models = ["LowRes", "standard", "HighRes"]
+time_refinement_models = ["large dt", "standard", "small dt"]
+variants = ["mesh_refinement", "time_refinement"]
+
+for v, models in zip(variants, [mesh_refinement_models, time_refinement_models]):
+    os.makedirs(f"plots/{v}/", exist_ok=True)
 
 
-for m in meshes:
-    df[f"{m}_rel"] = df[m] / df["standard"]
+    results = dict()
+    for m in models:
+        md = dict()
+        tm = transport_model[m]
+        tmconfig = read_config(f"configfiles/{tm}.yml")
+        actual_mesh = Path(tmconfig["mesh"])
+        print(actual_mesh)
+        # get mesh stats
+        md.update(read_config(actual_mesh.with_suffix(".yml")))
+        # get CSF flow stats
+        csfm = csf_flow_model[m]
+        md.update(read_config(f"results/csf_flow/{csfm}/metrics.yml"))
+        # get PVS flow stats
+        md.update(read_config(f"results/csf_flow/{csfm}/pvs_metrics.yml"))
+        # get cardiac PVS flow stats
+        cardiac_csf = read_config(f"results/csf_flow/cardiac_{csfm}/metrics.yml")
+        md.update({f"cardiac_{k}":v for k,v in cardiac_csf.items()})
+        # get mean concentrations
+        md.update(read_config(f"results/{tm}/mean_concentrations.yml"))
+        trmetrics = read_config(f"results/{tm}/{tm}_metrics.yml")
+        md.update({"Rmax":trmetrics["R_max"],"Rmean":trmetrics["R_mean"],
+                "Rmin":trmetrics["R_min"] })
+        for dom in ["csf", "art", "ven"]:
+            md.update({f"{dom}_max": max(trmetrics[f"{dom}_max"]),
+                    f"{dom}_min": min(trmetrics[f"{dom}_min"]),
+                    f"rel_undershot_{dom}": abs(min(np.array(trmetrics[f"{dom}_min"]) /
+                                                np.array(trmetrics[f"{dom}_max"]) )) },
+                    )
+        results[m] = md
 
-dfrel = df[[f"{m}_rel" for m in meshes]]
+    df = pd.DataFrame(results)
+    dfstr = df.applymap('{:,.3g}'.format)
+    fig, ax = plt.subplots(figsize=(6,22))
+    ax.axis('tight')
+    ax.axis('off')
+    the_table = ax.table(cellText=dfstr.values,
+                            rowLabels=dfstr.index,
+                            colLabels=dfstr.columns,
+                            loc="center")
+    fig.tight_layout()
+    fig.savefig(f"plots/{v}/table.png")
 
-plt.figure()
-#dfrel = dfrel[~dfrel.index.isin(["ven_min", "rel_undershot_ven"])]
-ax = dfrel.plot(kind="bar", figsize=(40,4), ylim=(0, 5))
-pps = ax.containers[1]
-for i, p, val in zip(range(len(pps)), pps, df["standard"].values):
-   height = p.get_height()
-   ax.annotate('{0:6.2g}'.format(val),
-      xy=(p.get_x() + p.get_width() / 2, height),
-      xytext=(0, 3 + 2*(i%2)), # 3 points vertical offset
-      textcoords="offset points",
-      fontsize=6,
-      ha='center', va='bottom')
 
-plt.tight_layout()
-plt.savefig("plots/mesh_refinement/mesh_bars.png")
+    for m in models:
+        df[f"{m}_rel"] = df[m] / df["standard"]
 
-# investigate undershoots
-plt.figure()
+    dfrel = df[[f"{m}_rel" for m in models]]
 
-for m in meshes:
-    tm = transport_model[m]
-    tmconfig = read_config(f"configfiles/{tm}.yml")
-    trmetrics = read_config(f"results/{tm}/{tm}_metrics.yml")
-    csf_max = trmetrics["csf_max"]
-    csf_min = trmetrics["csf_min"]
-    times = np.arange(0, tmconfig["T"], tmconfig["dt"]) / (60*60)
-    plt.plot(times, csf_max, label=f"max {m}")
-    plt.plot(times, csf_min, label=f"min {m}")
-plt.xlabel("time (h)")
-plt.ylabel("tracer concentration (mol/L)")
-plt.legend()
-plt.savefig(f"plots/mesh_refinement/undershoots.png")
+    plt.figure()
+    dfrel = dfrel[~dfrel.index.isin(["rel_undershot_csf","rel_undershot_art", "rel_undershot_ven"])]
+    ax = dfrel.plot(kind="bar", figsize=(40,4), ylim=(0, 5))
+    pps = ax.containers[1]
+    for i, p, val in zip(range(len(pps)), pps, df["standard"].values):
+        height = p.get_height()
+        ax.annotate('{0:6.2g}'.format(val),
+            xy=(p.get_x() + p.get_width() / 2, height),
+            xytext=(0, 3 + 2*(i%2)), # 3 points vertical offset
+            textcoords="offset points",
+            fontsize=6,
+            ha='center', va='bottom')
+
+    plt.tight_layout()
+    plt.savefig(f"plots/{v}/mesh_bars.png")
+
+    # investigate undershoots
+
+    for dom in ["csf","art", "ven"]:
+        plt.figure()
+
+        for m in models:
+            tm = transport_model[m]
+            tmconfig = read_config(f"configfiles/{tm}.yml")
+            trmetrics = read_config(f"results/{tm}/{tm}_metrics.yml")
+            dmax = trmetrics[f"{dom}_max"]
+            dmin = trmetrics[f"{dom}_min"]
+            times = np.arange(0, tmconfig["T"], tmconfig["dt"]) / (60*60)
+            plt.plot(times, dmax, label=f"max {m}")
+            plt.plot(times, dmin, label=f"min {m}")
+        plt.xlabel("time (h)")
+        plt.ylabel("tracer concentration (mol/L)")
+        plt.legend()
+        plt.savefig(f"plots/{v}/{dom}_undershoots.png")
 
 
