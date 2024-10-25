c88b71f2e9f8ac649d20c397e67bcbf175a338fa
Marius Causemann <mariusca@simula.no>
2024-10-25 13:08:27 +0200

    smooth networks
    Former-commit-id: 958126920c082befc4a0d5e7393104880b24d48f

--- scripts/extract_vessels.py
+++ scripts/extract_vessels.py
@@ -1,9 +1,6 @@
 import nibabel
 import pyvista as pv
 import numpy as np
-from skimage.filters import frangi, sato
-import skimage.morphology as skim
-from skimage.measure import label
 import kimimaro
 import os
 import networkx as nx
@@ -27,39 +24,10 @@ def skel_to_mesh(skel):
 
 def skeletonize(img, resolution):
     skels = kimimaro.skeletonize(img, anisotropy=resolution,
-                             teasar_params={"scale": scale, "const": const,})
+                             teasar_params={"scale": scale, "const": const})
     joined_skels = kimimaro.join_close_components(skels.values(), radius=50)
     ds_skel = joined_skels.downsample(2)
     return ds_skel 
-
-def generate_splines(G, point_ratio=1):
-    G = G.to_undirected()
-    degrees = np.array([G.degree(i) for i in range(G.number_of_nodes())])
-    bifurcations = np.where(degrees > 2)[0]
-    points = np.array([G.nodes[i]["pos"] for i in range(G.number_of_nodes())])
-    Gwb = G.copy()
-    Gwb.remove_nodes_from(bifurcations)
-    splines = []
-
-    for seg in list(nx.connected_components(Gwb)):
-        g = G.subgraph(seg)
-        roots = [node for (node, order) in g.degree() if order<=1]
-        seg_nodes = seg
-        for r in roots:
-            seg_nodes = seg_nodes.union(set(G.neighbors(r)))
-        g = G.subgraph(seg_nodes)
-        newroots = [node for (node, order) in g.degree() if order==1]
-        spl = pv.Spline(points=points[list(nx.dfs_postorder_nodes(g, source=newroots[0]))],
-                         n_points=int(len(seg)*point_ratio))
-        splines.append(spl)
-    
-    for bi in bifurcations:
-        for nbi in G.neighbors(bi):
-            if nbi in bifurcations:
-                spl = pv.Spline(points=points[[bi, nbi]], n_points=int(2*point_ratio))
-                splines.append(spl)
-
-    return splines
 
 def as_networkx(skel, nroots):
     G = nx.DiGraph()
@@ -138,42 +106,117 @@ def as_tubes(splines):
         tubes.append(spl.tube(scalars="radius", absolute=True))
     return tubes
 
+def pvnetwork_to_polydata(netw):
+    poly = pv.PolyData(netw.points, lines=netw.cells)
+    for k, data in netw.point_data.items():
+        poly[k] = data
+    return poly
 
-scale = 0.5
-const = 0.5
+def pv_to_df(netw):
+    import dolfin as df
+    mesh = df.Mesh()
+    ed = df.MeshEditor()
+    ed.open(mesh, 'interval', 1, 3)
+    ed.init_vertices(netw.number_of_points)
+    cells = netw.cells.reshape((-1,3))
+    ed.init_cells(cells.shape[0])
 
-os.makedirs("../mesh/networks", exist_ok=True)
-os.makedirs("../plots", exist_ok=True)
+    for vid, v in enumerate(netw.points):
+        ed.add_vertex(vid, v)
+    for cid, c in enumerate(cells[:,1:]):
+        ed.add_cell(cid, c)
+    ed.close()
+    return mesh
 
-files = ["../data/pcbi.1007073.s007.nii.gz", "../data/pcbi.1007073.s008.nii.gz"]
+def seg_to_spline(seg, point_ratio):
+    degrees = np.array([len(seg.point_neighbors(i)) for i in range(seg.n_points)])
+    roots = np.where(degrees==1)[0]
+    previous = None
+    current = roots[0]
+    pointlist = [current]
+    while True:
+        nei = seg.point_neighbors(current)
+        if previous is not None:
+            nei.remove(previous)
+        if len(nei)==0:
+            break
+        previous = current
+        current = nei[0]
+        pointlist.append(current)
+    spl = pv.Spline(points=seg.points[pointlist], n_points=point_ratio*seg.n_points)
+    return spl.interpolate(seg, strategy="closest_point", radius=4e-3)
+
+def get_splines(netw, point_ratio=1):
+    from branch_marking import color_branches
+    segments, segids, _ = color_branches(pv_to_df(netw))
+    pvsegments = [netw.extract_cells(segments.array()[:]==si) for si in segids]
+    return [seg_to_spline(pvs, point_ratio) for pvs in pvsegments]
+
+def get_tubes(netw):
+    return as_tubes(get_splines(netw)).combine()
+
+def smooth_radius(netw):
+    r = netw["radius"]
+    rnew = np.ones_like(r)
+    for i in range(netw.n_points):
+        ns = netw.point_neighbors(i)
+        if len(ns)<=2:
+            rnew[i] = 0.5*r[i] + 0.5*(r[ns].mean())
+        else:
+            rnew[i] = max(r[ns + [i]])
+    netw["radius"] = rnew
+
+def smooth_coords(netw):
+    coords = netw.points
+    coordsnew = np.copy(coords)
+    for i in range(netw.n_points):
+        ns = netw.point_neighbors(i)
+        # only smooth inner points, leave end and bifurcation points
+        if len(ns)==2: 
+            coordsnew[i,:] = 0.5*coords[i,:] + 0.5*(coords[ns,:].mean(axis=0))
+    netw.points[:,:] = coordsnew
+
+scale = 1.5
+const = 3.0
+
+os.makedirs("mesh/networks", exist_ok=True)
+os.makedirs("plots", exist_ok=True)
+
+files = ["data/pcbi.1007073.s007.nii.gz", "data/pcbi.1007073.s008.nii.gz"]
 names = ["arteries", "venes"]
 nroots = [3, 50]
 
 for file, name, nr in zip(files, names, nroots):
     data = nibabel.load(file)
     resolution = data.header["pixdim"][1:4]
+    print(f"resolution: {resolution}")
     img = data.get_fdata().astype(int)
     skel = skeletonize(img, resolution)
     if name=="arteries":
         skel = skel.crop(Bbox([0, 0, 37], img.shape))
     G = as_networkx(skel, nroots=nr)
     orig_netw = nx_to_pv(G)
-    splines = generate_splines(G, point_ratio=3)
-    splines = [spl.interpolate(orig_netw, strategy="closest_point") for spl in splines]
+    orig_netw["radius"] *= mm2m
+    orig_netw.scale(mm2m, inplace=True)
+    remove_duplicate_cells(orig_netw)
+    orig_netw.save(f"mesh/networks/{name}.vtk")
+    splines = get_splines(orig_netw, point_ratio=3)
     lines = [pv.lines_from_points(spl.points) for spl in splines]
     for l, spl in zip(lines, splines):
         l["radius"] = spl["radius"]
         l["root"] = spl["root"]
-    smooth_netw = pv.MultiBlock(lines).combine().clean().scale(mm2m)
-    smooth_netw["radius"] *= mm2m
-    orig_netw["radius"] *= mm2m
-    orig_netw.scale(mm2m, inplace=True)
-    remove_duplicate_cells(smooth_netw)
-    remove_duplicate_cells(orig_netw)
-    orig_netw.save(f"../mesh/networks/{name}.vtk")
-    smooth_netw.save(f"../mesh/networks/{name}_smooth.vtk")
-    netw_tubes = as_tubes(splines).combine().scale(mm2m)
-    netw_tubes["radius"] *= mm2m
-    netw_tubes.save(f"../mesh/networks/{name}_tubes.vtk")
-    netw_tubes.plot(off_screen=True, screenshot=f"../plots/{name}_network.png", zoom=1.6)
+    mean_radius = [l["radius"].mean() for l in lines]
+    order_by_radius = np.flip(np.argsort(mean_radius))
+    smooth_netw = pv.merge([lines[i] for i in order_by_radius],
+                            merge_points=True).cast_to_unstructured_grid()
+    for i in range(3):
+        smooth_radius(smooth_netw)
+    for i in range(3):
+        smooth_coords(smooth_netw)
+    smooth_netw.save(f"mesh/networks/{name}_smooth.vtk")
+    netw_tubes = get_tubes(smooth_netw)
+    netw_tubes.save(f"mesh/networks/{name}_tubes.vtk")
+    print(smooth_netw.n_points)
+    #netw_tubes.plot(off_screen=True, screenshot=f"plots/{name}_network.png", zoom=1.6)
     #plot_radii([s for s in splines if s.number_of_points > 4], f"../plots/{name}_arc_radii.png")
+
