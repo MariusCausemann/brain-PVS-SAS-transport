5f3706b887ef8bea96aa28846663949fb5c44e5c
Marius Causemann <mariusca@simula.no>
2024-10-25 13:09:50 +0200

    update locations
    Former-commit-id: d9acbdd4ae2ccccedf54f2826833092d6a55ddd9 [formerly 04870aeb2462b9e5a8fb853a5892da852bf53cd6]
    Former-commit-id: c68116c60f44a77ec6b0091d7ff92e4286cbfe14

--- scripts/label_arteries.py
+++ scripts/label_arteries.py
@@ -1,15 +1,22 @@
 import pyvista as pv
 import numpy as np
 import seaborn as sns
-pointlabels = [("ACA", (0.0797341, 0.132915, 0.0937325)),
-               ("ICA1",(0.0942801, 0.11788, 0.0505869)),
-               ("ICA2",(0.0679531, 0.118951, 0.0485859)),
-               ("MCA1",(0.10513, 0.114464, 0.0672847)),
-               ("MCA2",(0.0640877, 0.114662, 0.0613344)),
-               ("BA",(0.0845697, 0.105035, 0.0450061)),
-               ("PCA1",(0.0719072, 0.0640646, 0.063061)),
-               ("PCA2",(0.0969292, 0.0740488, 0.0658269)),
+pointlabels = [("BA",(0.084971, 0.104599, 0.0424848)),
+               ("ICA-R",(0.0965484, 0.112456,0.0413785)),
+               ("ICA-L",(0.0695824, 0.111343, 0.0415065)),
+               ("MCA-R",(0.0975131, 0.110452, 0.0626856)),
+               ("MCA-L",(0.063242, 0.115508, 0.0613588)),
+               ("MCA2-R",(0.127254, 0.118198, 0.0756301)),
+               ("MCA2-L",(0.0360319, 0.114267, 0.0717319)),
+               ("PCA-R",(0.100641, 0.0843866, 0.0609018)),
+               ("PCA-L",(0.0686598, 0.0831056, 0.0568451)),
+               ("ACA", (0.0795614, 0.132763, 0.0892396)),
+               ("PER-R", (0.123073, 0.101526, 0.135256)),
+               ("PER-L", (0.0401122, 0.0939796, 0.13324))
             ]
+
+def get_tangent(v, n):
+    return v - np.dot(np.dot(v,n), n)
 
 if __name__=="__main__":
    filename = "plots/labeled_arteries.png"
@@ -18,8 +25,8 @@ if __name__=="__main__":
 
    art["radius"] *= 1e3
    col = "fuchsia"      
-   arrow_length = 0.03
-
+   arrow_length = 0.015
+   camera_direction = (-0.256, -0.958, -0.122)
    points = [p for l, p in pointlabels]
    labels = [l for l, p in pointlabels]
    bar_args=dict(title="radius (mm)", vertical=False,
@@ -34,6 +41,7 @@ if __name__=="__main__":
    label_coords = []
    for i, (l, p) in enumerate(pointlabels):
       dir = np.array(p) - center
+      dir = get_tangent(dir, camera_direction)
       dir /= np.linalg.norm(dir)
       arr = pv.Arrow(start=np.array(p), direction=dir, scale=arrow_length,
                      tip_length=1e-3, tip_radius=5e-3, shaft_radius=1e-2)
@@ -48,3 +56,5 @@ if __name__=="__main__":
    pl.camera.elevation += 7
    pl.camera.zoom(1.6)
    pl.screenshot(filename, transparent_background=False)
+   print(pl.camera.direction)
+   pl.export_html(filename.replace("png", "html"))
