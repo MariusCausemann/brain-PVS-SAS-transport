217f1f71b8ee6b4e03b4e168dfcd15ae02c454bc
Marius Causemann <mariusca@simula.no>
2024-10-25 13:10:16 +0200

    add boundary plot
    Former-commit-id: 6cdc41112354257310db07000218759c9d5c89a8 [formerly 7c43b895e9221c8986c7cec8d4a7af7c70d1b020]
    Former-commit-id: a07380a54b501bf5d8f8b4b76a149a6e05503148

---
+++ scripts/plot_boundaries.py
@@ -0,0 +1,60 @@
+import pyvista as pv
+import os
+from mark_and_refine_mesh import (PIA_ID, LV_INTERF_ID, SPINAL_OUTLET_ID,
+LOWER_SKULL_ID, UPPER_SKULL_ID, SPINAL_CORD_ID)
+import seaborn as sns
+import numpy as np
+from cmap import Colormap
+
+grid = pv.read_meshio("mesh/standard/standard_facets.xdmf")
+dirname = "plots/meshplots/"
+os.makedirs(dirname, exist_ok=True)
+
+bds = {"SK-U":UPPER_SKULL_ID,"SK-L": LOWER_SKULL_ID,
+       "SSAS":SPINAL_OUTLET_ID,
+        #"SC": SPINAL_CORD_ID, 
+        "LV":LV_INTERF_ID,  "Pia":PIA_ID,}
+clips = ["Pia", "SK-L", "SK-U"]
+
+bar_args=dict(title="", vertical=False, height=0.08, width=0.8, position_x=0.1,
+                        position_y=-0.05, title_font_size=52,
+                        bold=False, font_family="times",
+                        label_font_size=44)
+print(grid.array_names)
+bounds = list(grid.bounds)
+bounds[0] = (bounds[0] + bounds[1]) * 0.5
+bounds[2] = (bounds[2] + bounds[3]) * 0.4
+print(bounds)
+colors = sns.color_palette("twilight_shifted", n_colors=20, as_cmap=True)
+#colors = sns.diverging_palette(145, 300, s=60, as_cmap=True)
+
+meshes= []
+for i, (n, bid) in enumerate(bds.items()):
+    m = grid.extract_cells(grid["f"]==bid)
+    if n in clips:
+        m = m.clip() #m.clip_box(bounds)
+    m["l"] = np.full(m["f"].shape, n, dtype='<U10')
+    meshes.append(m)
+
+
+pl = pv.Plotter(off_screen=True, window_size=(1600, 1600))
+pl.add_mesh(grid.clip())
+pl.camera_position = 'yz'
+pl.camera.roll += 0
+pl.camera.azimuth += 30
+#pl.camera.elevation += 10
+pl.camera.zoom(1.2)
+camera = pl.camera.copy()
+
+
+pl = pv.Plotter(off_screen=True, window_size=(1600, 1600))
+pl.add_mesh(pv.merge(meshes), scalars="l", show_scalar_bar=True, 
+            scalar_bar_args=bar_args, 
+            cmap=Colormap('cmocean:curl_pink_r'),
+            #cmap=Colormap('vispy:pugr')
+            )
+pl.scalar_bar.SetTextPosition(0)
+pl.camera = camera
+pl.screenshot(f"{dirname}/boundaries.png",
+transparent_background=True)
+print(pl.camera.position)
