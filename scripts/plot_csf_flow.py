fd682abad4eecc4a93fec0e0144fcd585d46ddd2
Marius Causemann <mariusca@simula.no>
2024-11-01 15:40:11 +0100

    update plots
    Former-commit-id: fbf90e44530728f0007cc425baebdb52923cf94a

--- scripts/plot_csf_flow.py
+++ scripts/plot_csf_flow.py
@@ -12,10 +12,12 @@ from generate_synthseg_mesh import CSFID, LVID, V34ID
 import matplotlib.pyplot as plt
 from matplotlib.ticker import PercentFormatter
 import seaborn as sns
+from plot_subdomains import get_camera
+from vtk.util.numpy_support import numpy_to_vtk
 
 m2mum = 1e6
 
-def velocity_histo(sm, u, filename):
+def velocity_histo(sm, u, filename, vscale, vunit):
     set_plotting_defaults()
     DG0 = sm.function_space()
     umag = project(sqrt(inner(u,u)), DG0,
@@ -27,33 +29,38 @@ def velocity_histo(sm, u, filename):
     nbins = 20
     colors = sns.color_palette("crest", n_colors=3)
     for ax, name, idx, col in zip(axes, ["SAS", "LV", "V3 & V4"], indices, colors):
-        uvals = umag.vector()[idx] * m2mum
+        uvals = umag.vector()[idx] * vscale
         xlims = (0, np.percentile(uvals, 92))
         ax.hist(uvals, bins=nbins, weights=cellvol[idx] / cellvol[idx].sum(),
                  range=xlims,
                   color=col, alpha=0.7, label=name)
         mean = np.average(uvals, weights=cellvol[idx])
         ax.axvline(mean, 
-                   color="black",ymax=0.6, label=f"{mean:.1f} µm/s \n (mean)")
+                   color="black",ymax=0.6, label=f"{mean:.1f} {vunit} \n (mean)")
         #ax.axvline(np.max(uvals), ls="dotted",
         #           color="black",ymax=0.6, label=f"{max*1e-3:.1f} mm/s")
-        ax.set_xlabel("velocity (µm/s)")
+        ax.set_xlabel(f"velocity ({vunit})")
         ax.set_ylabel("frequency")
         plt.tight_layout()
         ax.set_xlim(xlims)
         ax.legend(frameon=False, loc="lower left", alignment="right",
                   borderaxespad=0, handlelength=1.3, bbox_to_anchor=[0.2, 0.7])
         ax.yaxis.set_major_formatter(PercentFormatter(1, decimals=0))
-    plt.savefig(filename)
+    plt.savefig(filename, bbox_inches='tight')
 
 def from_k3d(colorlist):
     cm = np.array(colorlist).reshape(-1, 4)[:, 1:]
     return matplotlib.colors.LinearSegmentedColormap.from_list("", cm)
 
 cardiac_config = dict(pcmap="balance", 
-                    vcmap="turbo")
+                    vcmap="inferno", vscale=1e2, pscale=1, 
+                    vunit="cm/s", punit="Pa",
+                    vticks= [1e-2, 0.1, 1, 5],
+                    vmin=1e-2, vbar_fmt="%.1f")
 prod_config = dict(pcmap=from_k3d([1]*4 + pcm.Blue___Green___Orange), 
-                    vcmap="inferno")
+                    vcmap="inferno", vscale=1e3, pscale=1e3,  
+                    vunit="mm/s", punit="mPa", vticks= [1e-4, 1e-3, 1e-2, 1e-1],
+                    vmin=1e-4,  vbar_fmt="%.3f")
 
 def plot_csf_flow(dirname: str):
     filename = f"{dirname}/flow.hdf"
@@ -63,8 +70,7 @@ def plot_csf_flow(dirname: str):
         f.read(mesh, "mesh", False)
         p_elem = eval(f.attributes("/pressure").to_dict()["signature"])
         v_elem = eval(f.attributes("/velocity").to_dict()["signature"])
-        DG =     velocity_histo(sm, v, f"{dirname}/velocity_histo.png")
-        FunctionSpace(mesh, p_elem)
+        DG = FunctionSpace(mesh, p_elem)
         V = FunctionSpace(mesh, v_elem)
         DG0 = FunctionSpace(mesh, "DG", 0)
         p, v, sm = Function(DG), Function(V), Function(DG0)
@@ -73,8 +79,6 @@ def plot_csf_flow(dirname: str):
         f.read(sm, "label")
     p = interpolate(p, FunctionSpace(mesh, "DG", v_elem.degree()))
 
-    from IPython import embed; embed()
-    velocity_histo(sm, v, f"{dirname}/velocity_histo.png")
     topology, cell_types, x = create_vtk_structures(V)
     grid = pv.UnstructuredGrid(topology, cell_types, x)
     grid["v"] = v.vector()[:].reshape(-1, 3)
@@ -82,8 +86,15 @@ def plot_csf_flow(dirname: str):
     if "cardiac" in dirname:
         grid["p"] *= (1 + alpha**2 / 8)
         config = cardiac_config
+        velocity_histo(sm, v, f"{dirname}/cardiac_velocity_histo.png", vscale=1e3,
+                       vunit="mm/s")
     else:
         config = prod_config
+        velocity_histo(sm, v, f"{dirname}/prod_velocity_histo.png", vscale=1e6,
+                       vunit="μm/s")
+
+    grid["v"] *= config["vscale"]
+    grid["p"] *= config["pscale"]
 
     gridsl = grid.slice(generate_triangles=True)
     streamlines = gridsl.streamlines(
@@ -91,35 +102,40 @@ def plot_csf_flow(dirname: str):
         source_radius=0.08,
     max_step_length=0.5, vectors="v",
     surface_streamlines=True, interpolator_type="p", 
-    compute_vorticity=False, max_time=10, n_points=40000,
+    compute_vorticity=False, n_points=40000, 
+    terminal_speed=3e-4, max_steps=200,
     )
 
-    p_bar_args=dict(title="p (Pa)", vertical=False,
+    p_bar_args=dict(title=f"p ({config['punit']})", vertical=False,
                         height=0.08, width=0.6, position_x=0.2,
                         position_y=-0.0, title_font_size=52,
                         bold=False, font_family="times",
-                        label_font_size=44, fmt="%.3g")
+                        label_font_size=44, fmt="%.1f")
     v_bar_args = p_bar_args.copy()
-    v_bar_args.update(dict(title="u (m/s)", vertical=True),
-                            height=0.4, width=0.08, position_x=0.85,
-                            position_y=0.08)
+    v_bar_args.update(dict(title=f"u ({config['vunit']})", vertical=True),
+                            height=0.4, width=0.08, position_x=0.88,
+                            position_y=0.02,
+                            fmt=config["vbar_fmt"])
 
     pl = pv.Plotter(off_screen=True, window_size=(1600, 1600))
-    pl.add_mesh(grid.clip(), scalars="p", clim=[0, np.round(grid["p"].max(),3)],
+    pl.add_mesh(grid.clip(), scalars="p", clim=[0, np.round(grid["p"].max(),1)],
         cmap=config["pcmap"],
         scalar_bar_args=p_bar_args)
     vmax = np.round(np.linalg.norm(grid["v"], axis=1).max(), 3)
-    pl.add_mesh(streamlines, line_width=2, cmap=config["vcmap"], clim=[0, vmax],
-    scalar_bar_args=v_bar_args)
+    pl.add_mesh(streamlines, line_width=2, cmap=config["vcmap"],
+                 clim=[config["vmin"], vmax],show_scalar_bar=False,
+                log_scale=True)
+    
+    bar = pl.add_scalar_bar(**v_bar_args)
+    ticks = config["vticks"] + [np.round(vmax, 3)]
+    bar.SetCustomLabels(numpy_to_vtk(ticks))
+    bar.SetUseCustomLabels(True)
     pl.camera_position = 'yz'
-    pl.camera.roll += 0
-    pl.camera.azimuth += 30
-    pl.camera.elevation += 10
-    pl.camera.zoom(1.2)
-    print(pl.camera.position)
-    print(pl.camera.focal_point)
+    pl.camera = get_camera(grid)
     pl.screenshot(f"{dirname}/csf_v.png",
     transparent_background=True)
+
+    #from IPython import embed; embed()
 
 if __name__ == "__main__":
     typer.run(plot_csf_flow)
--- scripts/plot_subdomains.py
+++ scripts/plot_subdomains.py
@@ -4,40 +4,45 @@ from generate_synthseg_mesh import PARID, CSFID, LVID,V34ID
 import seaborn as sns
 import numpy as np
 
-grid = pv.read_meshio("mesh/standard/standard.xdmf")
-dirname = "plots/meshplots/"
-os.makedirs(dirname, exist_ok=True)
+def get_camera(grid):
+    pl = pv.Plotter(off_screen=True, window_size=(1600, 1600))
+    pl.add_mesh(grid)
+    pl.camera_position = 'yz'
+    pl.camera.roll += 0
+    pl.camera.azimuth += 30
+    #pl.camera.elevation += 10
+    pl.camera.zoom(1.4)
+    return pl.camera.copy()
 
-PAR = grid.extract_cells(grid["label"]==PARID)
-CSF = grid.extract_cells(grid["label"]==CSFID)
-V34 = grid.extract_cells(grid["label"]==V34ID)
-LV = grid.extract_cells(grid["label"]==LVID)
 
-CSF["l"] = np.full(CSF["label"].shape, " SAS", dtype='<U10')
-V34["l"] = np.full(V34["label"].shape, "V3 & V4", dtype='<U10')
-LV["l"] = np.full(LV["label"].shape, "LV", dtype='<U10')
+if __name__=="__main__":
+    grid = pv.read_meshio("mesh/standard/standard.xdmf")
+    dirname = "plots/meshplots/"
+    os.makedirs(dirname, exist_ok=True)
 
-comb = pv.merge([CSF.clip(), V34, LV])
+    PAR = grid.extract_cells(grid["label"]==PARID)
+    CSF = grid.extract_cells(grid["label"]==CSFID)
+    V34 = grid.extract_cells(grid["label"]==V34ID)
+    LV = grid.extract_cells(grid["label"]==LVID)
 
-bar_args=dict(title="", vertical=False, height=0.08, width=0.6, position_x=0.2,
-                        position_y=-0.05, title_font_size=52,
-                        bold=False, font_family="times",
-                        label_font_size=44)
+    CSF["l"] = np.full(CSF["label"].shape, " SAS", dtype='<U10')
+    V34["l"] = np.full(V34["label"].shape, "V3 & V4", dtype='<U10')
+    LV["l"] = np.full(LV["label"].shape, "LV", dtype='<U10')
 
-pl = pv.Plotter(off_screen=True, window_size=(1600, 1600))
-pl.add_mesh(CSF.clip())
-pl.camera_position = 'yz'
-pl.camera.roll += 0
-pl.camera.azimuth += 30
-#pl.camera.elevation += 10
-pl.camera.zoom(1.2)
-camera = pl.camera.copy()
+    comb = pv.merge([CSF.clip(), V34, LV])
 
-pl = pv.Plotter(off_screen=True, window_size=(1600, 1600))
-colors = sns.color_palette("Blues", n_colors=3)
-pl.add_mesh(comb, scalars="l", show_scalar_bar=True, cmap="Blues",
-            scalar_bar_args=bar_args)
-pl.scalar_bar.SetTextPosition(0)
-pl.camera = camera
-pl.screenshot(f"{dirname}/subdomains.png",
-transparent_background=True)
+    bar_args=dict(title="", vertical=False, height=0.08, width=0.6, position_x=0.2,
+                            position_y=-0.05, title_font_size=52,
+                            bold=False, font_family="times",
+                            label_font_size=44)
+
+    pl = pv.Plotter(off_screen=True, window_size=(1600, 1600))
+    colors = sns.color_palette("Blues", n_colors=3)
+    pl.add_mesh(comb, scalars="l", show_scalar_bar=True, cmap="Blues",
+                scalar_bar_args=bar_args)
+    pl.add_mesh(grid.extract_surface(), opacity=0.4, color=colors[0],
+                show_scalar_bar=False)
+    pl.scalar_bar.SetTextPosition(0)
+    pl.camera = get_camera(grid)
+    pl.screenshot(f"{dirname}/subdomains.png",
+    transparent_background=True)
