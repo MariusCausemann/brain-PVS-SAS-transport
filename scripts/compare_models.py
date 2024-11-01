836a7654b55103b50ca242405de84643e8c89973
Marius Causemann <mariusca@simula.no>
2024-11-01 15:37:53 +0100

    update plots
    Former-commit-id: b4befa3a5a8582d3f3f586ad22ba20b6c2e7f810 [formerly 1430a4fd8cb911768f5528c7bd110f3a46157745]
    Former-commit-id: 785196394576f278f44440dc79e4e94526ccfa2c

--- scripts/compare_models.py
+++ scripts/compare_models.py
@@ -3,18 +3,23 @@ from mpl_toolkits.axes_grid1 import ImageGrid
 import numpy as np
 import typer
 from typing import List
-from plotting_utils import time_str, read_config, compute_ranges, compute_diff_ranges
-from generate_plot import plot_model
-from create_diff_plot import plot_model_diff
+from plotting_utils import (time_str, read_config, compute_ranges,
+                    get_result, clip_plot, isosurf_plot, timesurf_plot,
+                    compute_diff_ranges)
+import pyvista as pv
+from generate_synthseg_mesh import CSFID, CSFNOFLOWID, PARID, LVID, V34ID
+import matplotlib as mpl
 
 crop_dict = {"overview":lambda img : img[100:-10,140:-140],
              "overview_bottom":lambda img : img[100:-100,140:-140],
              "detail":lambda img : img,
             "isosurf":lambda img : img[100:-10,140:-140],
+            "timesurf":lambda img : img[100:-10,140:-140],
             "detail_bottom":lambda img : img,
             "isosurf_bottom":lambda img : img[100:-100,140:-140],
+            "timesurf_bottom":lambda img : img[100:-100,140:-140],
              }
-padding = {"overview":0, "detail":0.05, "isosurf":0}
+padding = {"overview":0, "detail":0.05, "isosurf":0, "timesurf":0}
 percentile = 95
 
 def compare_models(modela:str, modelb:str, type:str, cmax:float, diffmax:float, times:List[int]):
@@ -30,21 +35,62 @@ def compare_models(modela:str, modelb:str, type:str, cmax:float, diffmax:float, 
         rmin = np.min([r[0] for r in rangediff.values()])
         rmax = np.max([r[1] for r in rangediff.values()])
         diffmax = max(abs(rmin), abs(rmax))
-    for i, t in enumerate(times):
-        for j,m in enumerate([modela, modelb,"diff"]):
-            if m=="diff":
-                img = plot_model_diff(modela, modelb, t, type, cmax=diffmax)
-            else:
-                img = plot_model(m, t, type, cmax=cmax)
-            ax = grid.axes_row[i][j]
-            ax.axis('off')
-            if i + 1==len(times):
-                ax.imshow(crop_dict[type](img))
-            else:
-                ax.imshow(crop_dict[type + "_bottom"](img))
 
-        ax = grid.axes_row[i][0]
-        ax.text(-0.08, 0.5, f"{time_str(t)} h", rotation=90, va="center",
+    model_data = dict()
+    for j,m in enumerate([modela, modelb,"diff"]):
+        model_data[m] = dict()
+        for k in ["sas", "artery", "vein"]:
+            if m=="diff":
+                da = model_data[modela][k].copy()
+                db = model_data[modelb][k]
+                if da.n_points != db.n_points:
+                    db = da.sample(db, pass_point_data=False)
+                for t in times: da[f"c_{t}"] = da[f"c_{t}"] - db[f"c_{t}"]
+                model_data[m][k] = da
+            else:
+                conf = read_config(f"configfiles/{modela}.yml")
+                mesh = pv.read_meshio(conf["mesh"])
+                bg = mesh.extract_surface()
+                model_data[m][k] = get_result(m,k, times)
+        sas = model_data[m]["sas"]
+        art = model_data[m]["artery"].ctp()
+        ven = model_data[m]["vein"].ctp()
+        for netw in [art, ven]: netw["radius"] *= conf["pvs_ratio_artery"]
+        csf = sas.extract_cells(np.isin(sas["label"], [CSFID, LVID, V34ID, CSFNOFLOWID]))
+        par = sas.extract_cells(sas["label"]==PARID)
+        for t in times:par[f"c_{t}"] *= 0.2
+        comb = pv.merge([csf, par], merge_points=False)
+        clim = (0, cmax)
+        cbar_title = "concentration (mol/l)"
+        for i, t in enumerate(times):
+            cmap = "tempo"
+            if m=="diff":
+                clim=(-diffmax, diffmax)
+                cbar_title = "concentration diff (mol/l)"
+                cmap="curl"
+            for i, t in enumerate(times):
+                title = f"time: {time_str(t)} h"
+                for g in [csf, art, ven, par, comb]: g.set_active_scalars(f"c_{t}")
+                if type=="overview":
+                    img = clip_plot(csf, par, [art, ven], None, title, clim=clim, 
+                            cmap=cmap, cbar_title=cbar_title)
+                if type=="isosurf":
+                    img = isosurf_plot(comb, [art], bg, None, title, clim=clim, 
+                            cmap=cmap, cbar_title=cbar_title)
+                if type=="timesurf":
+                    cmap = "colorbrewer:spectral"
+                    cbar_title = f"time (h)"
+                    img = timesurf_plot(comb, [art],bg, times,t, None, title, clim=clim, 
+                            cmap=cmap)
+                ax = grid.axes_row[i][j]
+                ax.axis('off')
+                if i + 1==len(times):
+                    ax.imshow(crop_dict[type](img))
+                else:
+                    ax.imshow(crop_dict[type + "_bottom"](img))
+
+                ax = grid.axes_row[i][0]
+                ax.text(-0.08, 0.5, f"{time_str(t)} h", rotation=90, va="center",
                     transform=ax.transAxes, fontsize=6)
         
     descr = [read_config(f"configfiles/{m}.yml")["description"] for m in [modela, modelb]]
--- scripts/compare_models_horizontal.py
+++ scripts/compare_models_horizontal.py
@@ -3,44 +3,85 @@ from mpl_toolkits.axes_grid1 import ImageGrid
 import numpy as np
 import typer
 from typing import List
-from plotting_utils import time_str, read_config, compute_ranges, compute_diff_ranges
-from generate_plot import plot_model
-import matplotlib as mpl
+from plotting_utils import (time_str, read_config, compute_ranges,
+                    get_result, clip_plot, isosurf_plot, timesurf_plot)
 import cmocean
+from generate_synthseg_mesh import CSFID, CSFNOFLOWID, PARID, LVID, V34ID
+import pyvista as pv
+from cmap import Colormap
+import nibabel
+from extract_vessels import np2pv
+import matplotlib as mpl
 
 crop_dict = {"overview":lambda img : img[80:-100,200:-200],
              "overview_bottom":lambda img : img[100:-100,140:-140],
              "detail":lambda img : img,
             "isosurf":lambda img : img[100:-10,140:-140],
+            "timesurf":lambda img : img[100:-10,140:-140],
             "detail_bottom":lambda img : img,
             "isosurf_bottom":lambda img : img[100:-100,140:-140],
+            "timesurf_bottom":lambda img : img[100:-100,140:-140],
              }
-padding = {"overview":0, "detail":0.05, "isosurf":0}
 percentile = 95
 
 def compare_models(modela:str, modelb:str, type:str, cmax:float, times:List[int]):
 
     fig = plt.figure(figsize=(len(times)*3, len(times)*3), frameon=True)
-    grid = ImageGrid(fig, 111, nrows_ncols=(2, len(times)), axes_pad=padding[type],
+    grid = ImageGrid(fig, 111, nrows_ncols=(2, len(times)),
                      cbar_location="right",
                      cbar_mode="single",
                      cbar_size="4%",
                      cbar_pad=0.1,)
+    
     if not cmax:
         rangea = compute_ranges(modela, times, percentile,)
         rangeb = compute_ranges(modelb, times, percentile)
         cmax = np.max([r[1] for r in rangea.values()] + [r[1] for r in rangeb.values()])
-
-    for i, t in enumerate(times):
-        for j,m in enumerate([modela, modelb]):
-            img = plot_model(m, t, type, cmax=cmax)
+    for j,m in enumerate([modela, modelb]):
+        conf = read_config(f"configfiles/{m}.yml")
+        mesh = pv.read_meshio(conf["mesh"])
+        bg = mesh.extract_surface()
+        t1data = nibabel.load("data/T1.nii.gz")
+        res = np.array(t1data.header["pixdim"][1:4])*1e-3
+        img = np2pv(t1data.get_fdata()[:,:-50, :], res, 
+                    origin=(0,0,0))
+        img.save("data/T1.vtk")
+        bg = img.slice()
+        sas = get_result(m, "sas", times)
+        art = get_result(m, "artery", times).ctp()
+        ven = get_result(m, "vein", times).ctp()
+        for netw in [art, ven]: netw["radius"] *= conf["pvs_ratio_artery"]
+        print(art.array_names)
+        csf = sas.extract_cells(np.isin(sas["label"], [CSFID, LVID, V34ID, CSFNOFLOWID]))
+        par = sas.extract_cells(sas["label"]==PARID)
+        for t in times:par[f"c_{t}"] *= 0.2
+        comb = pv.merge([csf, par], merge_points=False)
+        for i, t in enumerate(times):
+            title = f"time: {time_str(t)} h"
+            for g in [csf, art, ven, par, comb]: g.set_active_scalars(f"c_{t}")
+            cbar_title = "concentration (mol/l)"
+            if type=="overview":
+                cmap = "tempo"
+                img = clip_plot(csf, par, [art, ven], None, title, clim=(0, cmax), 
+                          cmap=cmap, cbar_title=cbar_title)
+            if type=="isosurf":
+                cmap = "tempo"
+                img = isosurf_plot(comb, [art], bg, None, title, clim=(0, cmax), 
+                          cmap=cmap, cbar_title=cbar_title)
+            if type=="timesurf":
+                cmap = "colorbrewer:spectral"
+                cbar_title = f"time (h)"
+                img = timesurf_plot(comb, [art],bg, times,t, None, title, clim=(0, cmax), 
+                          cmap=cmap)
+            
             ax = grid.axes_row[j][i]
             ax.axis('off')
             ax.imshow(crop_dict[type](img))
 
-        ax = grid.axes_row[0][i]
-        ax.text(0.5, 1.05, f"{time_str(t)} h", ha="center",
-                    transform=ax.transAxes, fontsize=12)
+            if j==0:
+                ax = grid.axes_row[0][i]
+                ax.text(0.5, 1.05, f"{time_str(t)} h", ha="center",
+                        transform=ax.transAxes, fontsize=12)
         
     descr = [read_config(f"configfiles/{m}.yml")["description"] for m in [modela, modelb]]
         
@@ -48,13 +89,16 @@ def compare_models(modela:str, modelb:str, type:str, cmax:float, times:List[int]
         ax = grid.axes_row[j][0]
         ax.text( -0.08, 0.5, m, va="center", transform=ax.transAxes, fontsize=12, rotation=90)
 
-    cmap = cmocean.cm.tempo
+    mplcmap = cmap=Colormap(cmap).to_matplotlib()
     norm = mpl.colors.Normalize(vmin=0, vmax=cmax)
-
-    grid[0].cax.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap),
-                         label="tracer concentration (mol/l)")
-
-
+    if type=="timesurf":
+        norm = mpl.colors.BoundaryNorm(np.arange(0, len(times) + 1), mplcmap.N)
+        cbar = grid[0].cax.colorbar(mpl.cm.ScalarMappable(norm=norm,cmap=mplcmap),
+                         label=cbar_title, ticks=np.arange(0, len(times)) + 0.5)
+        cbar.ax.set_yticklabels((np.array(times)/3600).astype(str))
+    else:
+        cbar = grid[0].cax.colorbar(mpl.cm.ScalarMappable(norm=norm,cmap=mplcmap),
+                                    label=cbar_title)
     plt.savefig(f"plots/comparisons/{modela}_{modelb}/{modela}_{modelb}_{type}_horizontal.png",
                  dpi=300, bbox_inches="tight",)
 
