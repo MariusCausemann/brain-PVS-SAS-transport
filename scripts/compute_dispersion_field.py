2d4e5baa8434a52949b40cdd2def8abc6fc6da65
Marius Causemann <mariusca@simula.no>
2024-11-27 20:10:55 +0100

    black and white plots / remove LV from histo
    Former-commit-id: ba4a98fe2bf20e8744032cd27268910d5bb17a8b [formerly ba4a98fe2bf20e8744032cd27268910d5bb17a8b [formerly 6fc92bd183090fb714c0546ff662d6b8c29ed464]]
    Former-commit-id: dd38e66de6cdf567fb5894668d60c58ae930053e
    Former-commit-id: 35f4e7273eafad4db1870f77462691c5fb138b1d

--- scripts/compute_dispersion_field.py
+++ scripts/compute_dispersion_field.py
@@ -9,17 +9,20 @@ from generate_synthseg_mesh import CSFID, LVID, V34ID
 import matplotlib.pyplot as plt
 import seaborn as sns
 from matplotlib.ticker import PercentFormatter
+import seaborn as sns
 
 def R_histo(sm, R, filename):
     set_plotting_defaults()
+    sns.set_context('paper', font_scale=1.8)
     DG0 = sm.function_space()
     cellvol = assemble(TestFunction(DG0)*dx)
-    ids = [CSFID, LVID, V34ID]
-    indices = [sm.vector()[:]==i for i in ids]  
-    fig, axes = plt.subplots(ncols=3, figsize=(7,3))
+    domains = [[CSFID], [V34ID],]
+    names = ["SAS", "V3 & V4"]
+    indices = [np.isin(sm.vector()[:], dom) for dom in domains]      
+    fig, axes = plt.subplots(ncols=len(domains), figsize=(7,3))
     nbins = 20
-    colors = sns.color_palette("crest", n_colors=3)
-    for ax, name, idx, col in zip(axes, ["SAS", "LV", "V3 & V4"], indices, colors):
+    colors = sns.color_palette("crest", n_colors=len(domains))
+    for ax, name, idx, col in zip(axes, names, indices, colors):
         Rvals = R.vector()[idx]
         xlims = (0, np.percentile(Rvals, 80))
         bins = np.logspace(start=np.log10(1e-1), stop=np.log10(200), num=nbins)
@@ -34,7 +37,7 @@ def R_histo(sm, R, filename):
         ax.set_xlabel(f"R")
         ax.set_ylabel("frequency")
         ax.set_xticks([0.1, 1, 10, 100])
-        plt.tight_layout()
+        #plt.tight_layout()
         #ax.set_xlim(xlims)
         ax.set_ylim((0, max(n)*1.5))
         ax.legend(frameon=False, loc="lower left", alignment="right",
@@ -53,32 +56,74 @@ def plot_R(R, filename):
     grid = pv.UnstructuredGrid(topology, cell_types, x)
     grid["R"] = R.vector()[:]
 
-    Rmin, Rmax = grid.clip()["R"].min(), grid.clip()["R"].max()
+    Rmin, Rmax = grid["R"].min(), grid["R"].max()
     bg = pv.read_meshio("mesh/standard/standard.xdmf")
-    pl = pv.Plotter(off_screen=True, window_size=(1600, 1600))
-    pl.add_mesh(bg.extract_cells(np.isin(bg["label"], [PARID])).clip(),
+    if "respiratory" in str(filename):
+        cmap = sns.blend_palette(["black", "white", "#092f8d"],as_cmap=True)
+    elif "cardiac" in str(filename):
+        cmap = "RdGy_r"
+
+    for c in ["white", "black"]:
+
+        pl = pv.Plotter(off_screen=True, window_size=(1600, 1600))
+        pl.add_mesh(bg.extract_cells(np.isin(bg["label"], [PARID])).clip(),
                 color="black")
-    pl.add_mesh(grid.clip(), log_scale=True, cmap="RdGy_r", 
-                show_scalar_bar=False, clim=(Rmin, Rmax))
-    bar = pl.add_scalar_bar(title="R", vertical=False,
-                    height=0.08, width=0.6, position_x=0.2,
-                    position_y=-0.0, title_font_size=52,
-                    bold=False, font_family="times",
-                    label_font_size=44, fmt="%.1f")
-    ticks = [Rmin, 1e-1,1, 10, 100]
-    bar.SetCustomLabels(numpy_to_vtk(ticks))
-    bar.SetUseCustomLabels(True)
-    pl.camera = get_camera(bg)
-    pl.screenshot(filename, transparent_background=True)
+
+        pl.add_mesh(grid.clip(), log_scale=True,cmap=cmap,
+                    show_scalar_bar=False, clim=(Rmin, Rmax))
+        bar = pl.add_scalar_bar(title="R", vertical=False,
+                        height=0.08, width=0.6, position_x=0.2,
+                        position_y=-0.0, title_font_size=64,
+                        bold=False, font_family="times", color=c,
+                        label_font_size=60, fmt="%.1f")
+        ticks = [Rmin, 1e-1,1, 10, int(np.floor(Rmax / 10) * 10)]
+        bar.SetCustomLabels(numpy_to_vtk(ticks))
+        bar.SetUseCustomLabels(True)
+        pl.camera = get_camera(bg)
+        path = Path(filename)
+        if c=="white":
+            path = path.parent / (path.stem + "_dark" + path.suffix)
+        pl.screenshot(path, transparent_background=True)
+
 
 rho = 993 # kg/m^3
 nu = 7e-7 # m^2/s
-omega = 2*np.pi
 h = 3e-3 / 2
-alpha = np.sqrt(h**2 * omega / nu)
+
+omega_cardiac = 2*np.pi * 1.0
+omega_respiratory = 2*np.pi * 0.25
+alpha_cardiac = np.sqrt(h**2 * omega_cardiac / nu)
+alpha_respiratory = np.sqrt(h**2 * omega_respiratory / nu)
+
+def test_sharp2019_SSAS():
+    delta_p = 45.3 # Pa
+    l = 0.1 # m
+    h = 3e-3 / 2
+    omega = omega_cardiac
+    gradp = delta_p / l
+    P = gradp /(rho*omega*nu/h)
+    assert np.isclose(P, 155.7, rtol=5e-2) # value from paper
+    alpha = np.sqrt(h**2 * omega_cardiac / nu)
+    R = P**2 / (alpha**3)
+
+def PVS_R():
+    delta_p = 2.5 # Pa
+    l = 0.1 # m
+    omega = 2*np.pi
+    gradp = delta_p / l
+    h = 1e-3 / 2
+    P = gradp /(rho*omega*nu/h)
+    alpha = np.sqrt(h**2 * omega / nu)
+    R = P**2 / (alpha**3)
+
 
 def get_dispersion_enhancement(csf_pressure_file:str, outfile:str):
-
+    if "cardiac" in csf_pressure_file:
+        omega = omega_cardiac
+        alpha = alpha_cardiac
+    elif "respiratory" in csf_pressure_file:
+        omega = omega_respiratory
+        alpha = alpha_respiratory
     assume_unsteady = True
     mesh = Mesh()
     with HDF5File(MPI.comm_world, csf_pressure_file,'r') as f:
@@ -91,9 +136,10 @@ def get_dispersion_enhancement(csf_pressure_file:str, outfile:str):
         sm = Function(DG0)
         f.read(sm, "label")
 
-    #p_cont = project(pressure_csf, V)
     gradp = sqrt(inner(grad(pressure_csf), grad(pressure_csf)))
     P = gradp /(rho*omega*nu/h)
+    print(f"alpha**2: {alpha**2}")
+
     if assume_unsteady:
         print(f"adjusting pressure by 1 + alpha**2 / 8 = {1 + alpha**2 / 8}")
         P *= (1 + alpha**2 / 8) # scale pressure with womersley
