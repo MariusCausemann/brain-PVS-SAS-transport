6f2de64bd7bc06fc063ba86c0418ae8b04b187de
Marius Causemann <mariusca@simula.no>
2025-06-06 10:03:22 +0200

    minor change

--- scripts/make_video.py
+++ scripts/make_video.py
@@ -11,11 +11,13 @@ import seaborn as sns
 
 def make_movie(modelname:str):
     config = read_config(f"configfiles/{modelname}.yml")
-    dt , T= config["dt"], config["T"]
+    dt , T= config["dt"]*config["output_frequency"], config["T"]
     #cmap = sns.blend_palette(["lightsteelblue", "orange", "darkviolet"],as_cmap=True)
     cmap = from_k3d(pcm.Yellow___Gray___Blue).reversed()
     clim = (0,2)
-    times = np.arange(0, T + dt, dt*config["output_frequency"])
+    N = 300
+    times = np.arange(0, T + dt, dt)
+    print(times)
     scalarbar_args = dict(color="white", title="concentration (mmol/l)", 
                           vertical=False, height=0.08, width=0.6,
                           position_x=0.2, position_y=0.05, 
@@ -31,8 +33,12 @@ def make_movie(modelname:str):
     for t in times:
         sas[f"c_{t}"] = np.where(sas[f"c_{t}"] > clim[1], 
                                  clim[1], sas[f"c_{t}"])
-    img = pv.create_grid(sas, dimensions=(300, 300, 300))
+    img = pv.create_grid(sas, dimensions=(N, N, N))
     img = img.sample(sas, progress_bar=True)
+    print("interpolating...")
+    for t in tqdm(times[:-1]):
+        img[f"c_{int(t + dt/2)}"] = 0.5 *(img[f"c_{t}"] + img[f"c_{t + dt}"])
+        arteries[f"c_{int(t + dt/2)}"] = 0.5 *(arteries[f"c_{t}"] + arteries[f"c_{t + dt}"])
 
     pl = pv.Plotter(off_screen=True, window_size=(1200, 1200))
     pl.set_background("black")
@@ -47,12 +53,15 @@ def make_movie(modelname:str):
     pl.add_mesh(arteries, scalars="c", cmap=cmap, 
                 clim=clim, show_scalar_bar=False,)
     pl.camera_position = 'xz'
-    pl.camera.zoom = 1.4
+    pl.camera.focal_point = np.array(skull.center) - np.array([0,0,0.011])
+    pl.camera.zoom(1.4)
+
     pl.show(auto_close=False) 
     pl.write_frame() 
 
     # generate frames
-    for t in tqdm(list(times[1:]) + [times[-1]]*24*10):
+    frametimes = np.arange(0, T + dt/2, dt/2, dtype=int)
+    for t in tqdm(list(frametimes[1:]) + [times[-1]]*24*10):
         pl.add_text(f"{int(t/3600)} h", name='time-label', 
                     color="lightgray", font_size=24)
         arteries["c"] = arteries[f"c_{t}"]
@@ -62,7 +71,7 @@ def make_movie(modelname:str):
                       show_scalar_bar=True, name="vol",
                       opacity_unit_distance=0.0002,
                       scalar_bar_args=scalarbar_args, mapper="gpu")
-        pl.camera.azimuth += 360 / len(times) * T/(24*60*60)
+        pl.camera.azimuth += 360 / len(frametimes) * T/(24*60*60)
         pl.write_frame()  
 
     pl.close()
