ff9aec4cb28f26d20b936e51e0cf3f1dcb35e43b
Marie E. Rognes <meg@simula.no>
2026-02-02 15:16:19 +0100

    Add outline of Story

--- scripts/movie_story/make_story_video.py
+++ scripts/movie_story/make_story_video.py
@@ -13,35 +13,69 @@
 import typer
 from pathlib import Path
 
-from moviepy.editor import TextClip, ColorClip, CompositeVideoClip
+from moviepy.editor import (TextClip, ColorClip, CompositeVideoClip,
+                            VideoFileClip, concatenate_videoclips) 
 
-def make_movie(framedir: str):
-    # This overview function simply collects all frames (images),
-    # connects them with text clips, and uses moviepy to 
-    
-    # Add all clips to this and composite video at the end
-    clips = []
+# Set background size and color
+width = 1920 # pixels
+height = 1080 # pixels
+black = (0, 0, 0)
 
-    # Set background size and color
-    width = 1920 # pixels
-    height = 1080 # pixels
-    black = (0, 0, 0)
-    
-    # Main cover
-    duration = 5.0
+
+def section_clip(title, duration):
     background = ColorClip(size=(width, height), color=black, duration=duration)
-
-    # Main cover 
-    title = "The brain's waterscape: in-silico molecular transport"
     clip = TextClip(title, fontsize=70, color="white", font="Arial-Bold",
                     method="label")
-    clip = clip.set_position('center').set_duration(5)
-    clip = clip.fadein(1.0) # This adds a 1-second fade-in from black 
-    clips.extend([background, clip])
-    
-    # Composite everything
-    video = CompositeVideoClip(clips)
+    clip = clip.set_position('center').set_duration(duration)
+    clip = clip.fadein(1.0).fadeout(1.0) # 1 sec fade-in and 1 sec fade-out
 
+    composite = CompositeVideoClip([background, clip])
+
+    return composite
+    
+def generate_full_movie(framedir: str, videofile: str):
+    
+    # A list of clips to be concatenated at the end
+    clips = []
+
+    # Main cover: 
+    title = "The brain's waterscape: in-silico molecular transport"
+    clip = section_clip(title, 5.0)
+    clips.append(clip)
+    
+    # Section #1
+    title = "Our subject: healthy male (26 y)"
+    clip = section_clip(title, 3.0)
+    clips.append(clip)
+
+    # Section #2
+    title = "From raw to segmented brain images"
+    clip = section_clip(title, 3.0)
+    clips.append(clip)
+    
+    # Section #3
+    title = "From segmented images to tetrahedral meshes"
+    clip = section_clip(title, 3.0)
+    clips.append(clip)
+
+    # Section #4
+    title = "From meshes to finite element simulation"
+    clip = section_clip(title, 3.0)
+    clips.append(clip)
+
+    if False:
+        # Add existing video
+        # This puts the video on a black background of the correct size
+        video_clip = VideoFileClip(videofile)
+        video_clip = video_clip.subclip(0, 3) # For more rapid testing
+        video_clip.set_position("center")
+        video_clip = CompositeVideoClip([video_clip], size=(width, height))
+        video_clip = video_clip.fadein(0.5)
+        clips.append(video_clip)
+    
+    # Concatenate everything
+    video = concatenate_videoclips(clips)
+    
     # Write final file
     filename = "insilico_transport.mp4"
     video.write_videofile(filename, fps=24)
@@ -50,8 +84,7 @@ def make_movie(framedir: str):
 
 if __name__ == "__main__":
 
-    typer.run(make_movie)
-
+    typer.run(generate_full_movie)
     
 # def add_heading(pl, heading, frames):
 #     # Show the heading for the given number of frames 
